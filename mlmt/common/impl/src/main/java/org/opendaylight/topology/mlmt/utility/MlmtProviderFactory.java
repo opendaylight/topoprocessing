153785b73d5abebab9a66aa540f0497e130772da
egargio <giorgio.garziano@ericsson.com>
2015-02-18 13:36:00 +0100

    Observer introductory implementation and directory structure update
    Change-Id: I26d22507884f0fdff3b4d9c4eae09035aa68353c
    Signed-off-by: egargio <giorgio.garziano@ericsson.com>

---
+++ mlmt/common/impl/src/main/java/org/opendaylight/topology/mlmt/utility/MlmtProviderFactory.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c)2015 Ericsson, AB. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.topology.mlmt.utility;
+
+import java.util.List;
+import java.util.HashMap;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.slf4j.Logger;
+
+public interface MlmtProviderFactory {
+
+    HashMap<String, List<MlmtTopologyProvider>> createProvidersMap(DataBroker dataBroker,
+            final Logger logger, MlmtOperationProcessor processor, String mlmtTopologyName);
+}
+
