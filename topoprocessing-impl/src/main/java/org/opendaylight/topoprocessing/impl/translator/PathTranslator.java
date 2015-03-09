37314b13d99444c39001ceb9d5386f4921f40060
Martin Uhlir <martin.uhlir@pantheon.sk>
2015-03-09 15:34:22 +0100

    Path Translation of yang string to YangInstanceIdentifier
    Change-Id: I61b2ae75f1beccec24d7775e836b1b26531252a9
    Signed-off-by: Martin Uhlir <martin.uhlir@pantheon.sk>

--- topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/translator/PathTranslator.java
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/translator/PathTranslator.java
@@ -8,17 +8,15 @@
 
 package org.opendaylight.topoprocessing.impl.translator;
 
-import java.util.Iterator;
-
+import org.opendaylight.topoprocessing.impl.util.GlobalSchemaContextHolder;
+import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.InstanceIdentifierBuilder;
-import org.opendaylight.yangtools.yang.model.api.DataNodeContainer;
 import org.opendaylight.yangtools.yang.model.api.DataSchemaNode;
 import org.opendaylight.yangtools.yang.model.api.Module;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 import com.google.common.base.Splitter;
-import com.google.common.base.Strings;
 
 
 /**
@@ -27,6 +25,9 @@ import com.google.common.base.Strings;
  */
 public class PathTranslator {
 
+    
+    private SchemaContext globalSchemaContext;
+
     /**
      * Translates yang path into {@link YangInstanceIdentifier}
      *
@@ -34,9 +35,41 @@ public class PathTranslator {
      *
      * @param yangPath path to target node
      * @return {@link YangInstanceIdentifier} leading to target node
+     * @throws IllegalArgumentException if yangPath is in incorrect format
+     * @throws Exception 
      */
-    public YangInstanceIdentifier translate(String yangPath) {
+    public YangInstanceIdentifier translate(String yangPath) throws IllegalArgumentException {
+        InstanceIdentifierBuilder builder = YangInstanceIdentifier.builder();
+        globalSchemaContext = GlobalSchemaContextHolder.getSchemaContext();
 
-        return null;
+        Iterable<String> yangPathSplitted = Splitter.on("/").split(yangPath);
+        for (String yangPathString : yangPathSplitted) {
+            int index = getSeparatorIndex(yangPathString, ':');
+            String moduleName = getModuleName(yangPathString, index);
+            Module module = globalSchemaContext.findModuleByName(moduleName, null);
+            String childName = getChildName(yangPathString, index);
+            DataSchemaNode dataChildByName = module.getDataChildByName(childName);
+            builder.node(dataChildByName.getQName());
+        }
+        return builder.build();
     }
+
+    private static int getSeparatorIndex(String s, char separator) {
+        int index = s.indexOf(separator);
+        if (index == -1) {
+            throw new IllegalArgumentException("Invalid format of yang path, ':' not found in " + s);
+        } else if (s.lastIndexOf(':') != index) {
+            throw new IllegalArgumentException("Invalid format of yang path, only one occurence of ':' is valid in " + s);
+        }
+        return index;
+    }
+
+    private static String getModuleName(String s, int index) {
+        return s.substring(0, index);
+    }
+
+    private static String getChildName(String yangPathString, int index) {
+        return yangPathString.substring(index, yangPathString.length());
+    }
+
 }
