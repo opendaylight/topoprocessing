ba05ff3da4c47a4a8a9efd18c7d1c0642eb156c4
Matus Marko <matus.marko@pantheon.sk>
2015-03-10 14:35:58 +0100

    UnderlayTopologyListener implementation
    Change-Id: I0f89b56c0e7f5687edbad3368c32d9d4b7c3c3a0
    Signed-off-by: Matus Marko <matus.marko@pantheon.sk>

---
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/listener/UnderlayTopologyListener.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.topoprocessing.impl.listener;
+
+import com.google.common.base.Optional;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataChangeListener;
+import org.opendaylight.topoprocessing.impl.operator.TopologyManager;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev130712.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
+
+import java.util.*;
+
+/**
+ * Listens on underlay topology changes
+ * @author matus.marko
+ */
+public class UnderlayTopologyListener implements DOMDataChangeListener {
+
+    public enum RequestAction {
+        CREATE, UPDATE, DELETE
+    }
+
+    private TopologyManager topologyManager;
+
+    private YangInstanceIdentifier yangInstanceIdConfiguration;
+
+    public UnderlayTopologyListener(TopologyManager topologyManager, YangInstanceIdentifier yangInstanceIdConfiguration) {
+        this.topologyManager = topologyManager;
+        this.yangInstanceIdConfiguration = yangInstanceIdConfiguration;
+    }
+
+    @Override
+    public void onDataChanged(AsyncDataChangeEvent<YangInstanceIdentifier, NormalizedNode<?, ?>> change) {
+        this.proceedChangeRequest(change.getCreatedData(), RequestAction.CREATE);
+        this.proceedChangeRequest(change.getUpdatedData(), RequestAction.UPDATE);
+        this.proceedDeletionRequest(change.getRemovedPaths());
+    }
+
+    private void proceedChangeRequest(Map<YangInstanceIdentifier, NormalizedNode<?, ?>> map, RequestAction requestAction) {
+        Map<YangInstanceIdentifier, NormalizedNode<?, ?>> resultEntries = new HashMap<>();
+        Iterator<Map.Entry<YangInstanceIdentifier, NormalizedNode<?, ?>>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<YangInstanceIdentifier, NormalizedNode<?, ?>> entry = iterator.next();
+            if (entry.getValue().getNodeType().equals(Node.QNAME))
+            {
+                Optional<NormalizedNode<?, ?>> node = NormalizedNodes.findNode(entry.getValue(), yangInstanceIdConfiguration);
+                if (node.isPresent()) {
+                    resultEntries.put(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+        // TODO - set entry to the TopologyManager with action
+    }
+
+    private void proceedDeletionRequest(Set<YangInstanceIdentifier> set) {
+        Iterator<YangInstanceIdentifier> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            YangInstanceIdentifier identifierOperational = iterator.next();
+            // TODO check if is functioning
+            if (identifierOperational.getLastPathArgument().getNodeType().equals(
+                    yangInstanceIdConfiguration.getLastPathArgument().getNodeType()))
+            {
+                // TODO - set entry to the TopologyManager with action
+            }
+        }
+    }
+}
