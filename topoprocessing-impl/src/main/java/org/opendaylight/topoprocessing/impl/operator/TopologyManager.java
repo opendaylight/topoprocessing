102777e903a46e6a625e94444ece431cfde70c18
Matus Marko <matus.marko@pantheon.sk>
2015-03-17 15:17:17 +0100

    Aggregation - removed data implementation
    Change-Id: Ief30073cc4ac0e13a4ed8193c79e72fbcde6eeab
    Signed-off-by: Matus Marko <matus.marko@pantheon.sk>

--- topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/operator/TopologyManager.java
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/operator/TopologyManager.java
@@ -58,8 +58,21 @@ public class TopologyManager {
 
     }
 
-    public void processDeletedChanges(YangInstanceIdentifier identifier) {
-
+    /**
+     * Delete node from Aggregation map
+     * @param identifier Yang Instance Identifier
+     * @param topologyId Topology Identification
+     */
+    public void processDeletedChanges(YangInstanceIdentifier identifier, final String topologyId) {
+        for (TopologyStore ts : topologyStores) {
+            if (ts.getId().equals(topologyId)) {
+                Map<YangInstanceIdentifier, PhysicalNode> physicalNodes = ts.getPhysicalNodes();
+                PhysicalNode physicalNode = physicalNodes.remove(identifier);
+                if (null != physicalNode) {
+                    // TODO remove from aggregator map
+                }
+            }
+        }
     }
 
     /**
