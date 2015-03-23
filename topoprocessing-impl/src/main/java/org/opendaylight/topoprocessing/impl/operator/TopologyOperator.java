4a26f2e9d104e21e3125046f65fa9d8b120b6e90
Matus Marko <matus.marko@pantheon.sk>
2015-03-23 10:31:07 +0100

    Aggregation map - provide methods for catching changes in aggregation map (create, update, remove)
    Change-Id: I8200444ee439c3b9087d623a24df2e43891c6acc
    Signed-off-by: Matus Marko <matus.marko@pantheon.sk>

--- topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/operator/TopologyOperator.java
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/operator/TopologyOperator.java
@@ -8,8 +8,35 @@
 
 package org.opendaylight.topoprocessing.impl.operator;
 
+import org.opendaylight.topoprocessing.impl.structure.PhysicalNode;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
  * @author matus.marko
  */
 public interface TopologyOperator {
+
+    /**
+     * Process newly created changes
+     * @param createdEntries
+     * @param topologyId
+     */
+    public void processCreatedChanges(Map<YangInstanceIdentifier, PhysicalNode> createdEntries, final String topologyId);
+
+    /**
+     * Process newly updated changes
+     * @param updatedEntries
+     * @param topologyId
+     */
+    public void processUpdatedChanges(Map<YangInstanceIdentifier, PhysicalNode> updatedEntries, String topologyId);
+
+    /**
+     * Process newly deleted changes
+     * @param identifiers Yang Instance Identifier
+     * @param topologyId Topology Identification
+     */
+    public void processRemovedChanges(ArrayList<YangInstanceIdentifier> identifiers, final String topologyId);
 }
