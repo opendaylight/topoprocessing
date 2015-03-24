878f1f5340a5a9b2eb773580119b7477dca23ae8
Martin Uhlir <martin.uhlir@pantheon.sk>
2015-03-24 16:43:42 +0100

    Filtrator implementation of methods for delegating filtration configuration
    Change-Id: If19bb99b88eea55d4f636652263e185a86e04646
    Signed-off-by: Martin Uhlir <martin.uhlir@pantheon.sk>

--- topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/operator/TopologyManager.java
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/operator/TopologyManager.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.opendaylight.topoprocessing.impl.structure.IdentifierGenerator;
-import org.opendaylight.topoprocessing.impl.structure.LogicalNode;
 import org.opendaylight.topoprocessing.impl.structure.PhysicalNode;
 import org.opendaylight.topoprocessing.impl.structure.TopologyStore;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.Equality;
@@ -22,7 +21,6 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.Unification;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.mapping.grouping.Mapping;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.network.topology.topology.correlations.Correlation;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.network.topology.topology.correlations.correlation.CorrelationType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.network.topology.topology.correlations.correlation.correlation.type.EqualityCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.network.topology.topology.correlations.correlation.correlation.type.NodeIpFiltrationCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.network.topology.topology.correlations.correlation.correlation.type.UnificationCase;
@@ -38,7 +36,9 @@ public class TopologyManager {
     private TopologyOperator filtrator = null;
     private IdentifierGenerator idGenerator = new IdentifierGenerator();
     private List<TopologyStore> topologyStores = new ArrayList<>();
+
     /**
+     * Process created changes
      * @param createdEntries
      * @param topologyId
      */
@@ -47,22 +47,22 @@ public class TopologyManager {
     }
 
     /**
-     * Process Deleted changes
-     * @param identifier Yang Instance Identifier
-     * @param topologyId Topology Identification
+     * Process removed changes
+     * @param identifiers which were removed in this change
+     * @param topologyId id of topology on which this method was called
      */
-    public void processDeletedChanges(ArrayList<YangInstanceIdentifier> identifiers, final String topologyId) {
+    public void processRemovedChanges(ArrayList<YangInstanceIdentifier> identifiers, final String topologyId) {
         aggregator.processRemovedChanges(identifiers, topologyId);
     }
 
     /**
-     * @param resultEntries
-     * @param underlayTopologyId
+     * Process updated changes
+     * @param updatedEntries
+     * @param topologyId
      */
     public void processUpdatedChanges(Map<YangInstanceIdentifier, PhysicalNode> updatedEntries,
             String topologyId) {
         aggregator.processUpdatedChanges(updatedEntries, topologyId);
-        
     }
 
     /**
@@ -75,39 +75,36 @@ public class TopologyManager {
             EqualityCase typeCase = (EqualityCase) correlation.getCorrelationType();
             List<Mapping> mappings = typeCase.getEquality().getMapping();
             iterateMappings(mappings);
-
             aggregator = new TopologyAggregator(correlation.getCorrelationItem(), topologyStores, idGenerator);
         }
         else if (correlation.getName().equals(Unification.class)) {
             UnificationCase typeCase = (UnificationCase) correlation.getCorrelationType();
             List<Mapping> mappings = typeCase.getUnification().getMapping();
             iterateMappings(mappings);
-
             //TODO Unification Operator
         }
         else if (correlation.getName().equals(NodeIpFiltration.class)) {
             NodeIpFiltrationCase typeCase = (NodeIpFiltrationCase) correlation.getCorrelationType();
             List<Filter> filters = typeCase.getNodeIpFiltration().getFilter();
             for (Filter filter : filters) {
-                initStore(filter.getUnderlayTopology());
             }
-
             filtrator = new TopologyFiltrator(correlation);
         }
     }
 
-    private void iterateMappings(List<Mapping> mappings ) {
+    private void iterateMappings(List<Mapping> mappings) {
         for (Mapping mapping : mappings) {
-            initStore(mapping.getUnderlayTopology());
+            initializeStore(mapping.getUnderlayTopology());
         }
     }
 
-    private void initStore(String id) {
+    private void initializeStore(String underlayTopologyId) {
         for (TopologyStore topologyStore : topologyStores) {
-            if (id == topologyStore.getId()) {
+            if (underlayTopologyId == topologyStore.getId()) {
                 return;
             }
         }
-        topologyStores.add(new TopologyStore(id, new HashMap<YangInstanceIdentifier, PhysicalNode>()));
+        topologyStores.add(new TopologyStore(underlayTopologyId,
+                new HashMap<YangInstanceIdentifier, PhysicalNode>()));
     }
 }
