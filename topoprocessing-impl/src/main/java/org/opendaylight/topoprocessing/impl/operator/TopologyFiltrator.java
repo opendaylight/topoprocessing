2c43bd0192cbef76e52ea34505be34361a1e40d8
Matus Marko <matus.marko@pantheon.sk>
2015-03-24 15:10:11 +0100

    Filtrator implementation
    Change-Id: I097e3290489a2455ad2244b985e9554de934bf95
    Signed-off-by: Matus Marko <matus.marko@pantheon.sk>

--- topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/operator/TopologyFiltrator.java
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/operator/TopologyFiltrator.java
@@ -8,14 +8,14 @@
 
 package org.opendaylight.topoprocessing.impl.operator;
 
-import org.opendaylight.topoprocessing.impl.structure.IdentifierGenerator;
-import org.opendaylight.topoprocessing.impl.structure.LogicalNode;
 import org.opendaylight.topoprocessing.impl.structure.PhysicalNode;
-import org.opendaylight.topoprocessing.impl.structure.TopologyStore;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.network.topology.topology.correlations.Correlation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.network.topology.topology.correlations.correlation.correlation.type.NodeIpFiltrationCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.network.topology.topology.correlations.correlation.correlation.type.node.ip.filtration._case.node.ip.filtration.Filter;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,35 +24,52 @@ import java.util.Map;
  */
 public class TopologyFiltrator implements TopologyOperator {
 
-    private Map<YangInstanceIdentifier,LogicalNode> aggregationMap = new AggregationMap();
-    private IdentifierGenerator idGenerator;
     private Correlation correlation;
-    private List<TopologyStore> topologyStores;
 
     /**
      * Constructor
      * @param correlation
      * @param topologyStores
      */
-    public TopologyFiltrator(Correlation correlation, List<TopologyStore> topologyStores,
-                             IdentifierGenerator idGenerator) {
+    public TopologyFiltrator(Correlation correlation) {
         this.correlation = correlation;
-        this.topologyStores = topologyStores;
-        this.idGenerator = idGenerator;
     }
 
     @Override
     public void processCreatedChanges(Map<YangInstanceIdentifier, PhysicalNode> createdEntries, String topologyId) {
-
+        Map<YangInstanceIdentifier, PhysicalNode> createdData = new HashMap<>();
+        NodeIpFiltrationCase typeCase = (NodeIpFiltrationCase) correlation.getCorrelationType();
+        List<Filter> filters = typeCase.getNodeIpFiltration().getFilter();
+        Filter filter = filters.get(0);
+        for (Map.Entry<YangInstanceIdentifier, PhysicalNode> changeEntry : createdEntries.entrySet()) {
+            //TODO check leaf node value
+            if (filter.getValue().getValue().equals(changeEntry.getValue().getLeafNode().getValue())) {
+                createdData.put(changeEntry.getKey(), changeEntry.getValue());
+            }
+        }
+        // TODO handle result
+        return; //createdData
     }
 
     @Override
     public void processUpdatedChanges(Map<YangInstanceIdentifier, PhysicalNode> updatedEntries, String topologyId) {
-
+        Map<YangInstanceIdentifier, PhysicalNode> updatedData = new HashMap<>();
+        NodeIpFiltrationCase typeCase = (NodeIpFiltrationCase) correlation.getCorrelationType();
+        List<Filter> filters = typeCase.getNodeIpFiltration().getFilter();
+        Filter filter = filters.get(0);
+        for (Map.Entry<YangInstanceIdentifier, PhysicalNode> changeEntry : updatedEntries.entrySet()) {
+            //TODO check leaf node value
+            if (filter.getValue().getValue().equals(changeEntry.getValue().getLeafNode().getValue())) {
+                updatedData.put(changeEntry.getKey(), changeEntry.getValue());
+            }
+        }
+        // TODO handle result
+        return; //updatedData
     }
 
     @Override
     public void processRemovedChanges(ArrayList<YangInstanceIdentifier> identifiers, String topologyId) {
-
+        //TODO handle result
+        return; // identifiers
     }
 }
