02ee0a3c7d0d86afebe941d111c0ad3053b281fc
Matus Marko <matus.marko@pantheon.sk>
2015-03-26 12:02:31 +0100

    TopologyAggregator Test
    - TopologyOperator interface changed due to that tests need the return value from operator methods
    
    Change-Id: I833ae4a72e5259f6872739734b136535d7c75ee6
    Signed-off-by: Matus Marko <matus.marko@pantheon.sk>

---
+++ topoprocessing-impl/src/test/org/opendaylight/topoprocessing/impl/operator/TopologyAggregatorTest.java
@@ -0,0 +1,32 @@
+package org.opendaylight.topoprocessing.impl.operator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author matus.marko
+ */
+public class TopologyAggregatorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testProcessCreatedChanges() throws Exception {
+
+    }
+
+    @Test
+    public void testProcessRemovedChanges() throws Exception {
+
+    }
+
+    @Test
+    public void testProcessUpdatedChanges() throws Exception {
+
+    }
+}
