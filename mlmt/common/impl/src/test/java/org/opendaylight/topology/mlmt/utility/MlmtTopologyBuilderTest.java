2120b7776e554648fa0586900ab23e0f1b6aaecc
giorgiogarziano <giorgio.garziano@ericsson.com>
2015-04-14 13:23:56 +0200

    Mlmt utility tests update
    Change-Id: I9d6c4b8cca140eac99526a6be867382de4f77182
    Signed-off-by: giorgiogarziano <giorgio.garziano@ericsson.com>

--- mlmt/common/impl/src/test/java/org/opendaylight/topology/mlmt/utility/MlmtTopologyBuilderTest.java
+++ mlmt/common/impl/src/test/java/org/opendaylight/topology/mlmt/utility/MlmtTopologyBuilderTest.java
@@ -9,6 +9,7 @@ package org.opendaylight.topology.mlmt.utility;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
@@ -45,12 +46,19 @@ import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.UnderlayTopology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.TopologyTypesBuilder;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Link;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPointBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPointKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Link;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.LinkBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.LinkKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.link.attributes.SourceBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.link.attributes.DestinationBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TpId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.LinkId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.node.attributes.SupportingNode;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
@@ -162,16 +170,21 @@ public class MlmtTopologyBuilderTest extends AbstractDataBrokerTest {
         builder.createUnderlayTopology(type, topologyInstanceId, underlayTopologyId);
     }
 
-    private void createNode(LogicalDatastoreType type, String nodeName) throws Exception {
-        final InstanceIdentifier<Topology> topologyInstanceId = mlmtTopologyInstanceId;
-        final TopologyId nodeTopologyId = new TopologyId("test");;
-        final NodeBuilder nodeBuilder = new NodeBuilder();
+    private NodeBuilder createNodeBuilder(String nodeName) {
+        NodeBuilder nodeBuilder = new NodeBuilder();
         NodeId nodeId = new NodeId(nodeName);
         NodeKey nodeKey = new NodeKey(nodeId);
         nodeBuilder.setKey(nodeKey);
         nodeBuilder.setNodeId(nodeId);
         nodeBuilder.setSupportingNode(Collections.<SupportingNode>emptyList());
-        nodeBuilder.setTerminationPoint(Collections.<TerminationPoint>emptyList());
+
+        return nodeBuilder;
+    }
+
+    private void createNode(LogicalDatastoreType type, String nodeTopologyName, String nodeName) throws Exception {
+        final InstanceIdentifier<Topology> topologyInstanceId = mlmtTopologyInstanceId;
+        final TopologyId nodeTopologyId = new TopologyId(nodeTopologyName);
+        final NodeBuilder nodeBuilder = createNodeBuilder(nodeName);
         builder.createNode(type, topologyInstanceId, nodeTopologyId, nodeBuilder.build());
 
         synchronized (waitObject) {
@@ -188,7 +201,283 @@ public class MlmtTopologyBuilderTest extends AbstractDataBrokerTest {
         assertEquals(checkTopologyId.getValue().toString(), mlmtTopologyId.getValue().toString());
         List<Node> lNode = rxTopology.getNode();
         assertFalse(lNode.isEmpty());
-        assertEquals(lNode.get(0).getNodeId().getValue().toString(), nodeName);
+
+        /*
+         * building array of node names to check against input one
+         */
+        List<String> lNodeNames = new ArrayList();
+        for (Node cNode : lNode) {
+            lNodeNames.add(cNode.getNodeId().getValue().toString());
+        }
+
+        assertTrue(lNodeNames.contains(nodeName));
+    }
+
+    private void deleteNode(LogicalDatastoreType type, String nodeTopologyName,
+            String nodeName) throws Exception {
+        final InstanceIdentifier<Topology> topologyInstanceId = mlmtTopologyInstanceId;
+        final TopologyId nodeTopologyId = new TopologyId(MLMT1);
+        final NodeKey nodeKey = new NodeKey(new NodeId(nodeName));
+
+        builder.deleteNode(type, topologyInstanceId, nodeKey);
+
+        synchronized (waitObject) {
+            waitObject.wait(5000);
+        }
+
+        ReadOnlyTransaction rTx = dataBroker.newReadOnlyTransaction();
+        Optional<Topology> optional = rTx.read(type, topologyInstanceId).get();
+        assertNotNull(optional);
+        assertTrue(MLMT1 + " topology not present", optional.isPresent());
+        Topology rxTopology = optional.get();
+        TopologyId checkTopologyId = rxTopology.getTopologyId();
+
+        assertEquals(checkTopologyId.getValue().toString(), mlmtTopologyId.getValue().toString());
+
+        List<Node> lNode = rxTopology.getNode();
+        assertFalse(lNode.isEmpty());
+
+        List<String> lNodeNames = new ArrayList();
+        for (Node cNode : lNode) {
+            String sNodeName = cNode.getNodeId().getValue().toString();
+            lNodeNames.add(sNodeName);
+        }
+
+        assertFalse(lNodeNames.contains(nodeName));
+    }
+
+    private void deleteLastNode(LogicalDatastoreType type, String nodeTopologyName,
+            String nodeName) throws Exception {
+        final InstanceIdentifier<Topology> topologyInstanceId = mlmtTopologyInstanceId;
+        final TopologyId nodeTopologyId = new TopologyId(MLMT1);
+        final NodeKey nodeKey = new NodeKey(new NodeId(nodeName));
+
+        builder.deleteNode(type, topologyInstanceId, nodeKey);
+
+        synchronized (waitObject) {
+            waitObject.wait(5000);
+        }
+
+        ReadOnlyTransaction rTx = dataBroker.newReadOnlyTransaction();
+        Optional<Topology> optional = rTx.read(type, topologyInstanceId).get();
+        assertNotNull(optional);
+        assertTrue(MLMT1 + " topology not present", optional.isPresent());
+        Topology rxTopology = optional.get();
+        TopologyId checkTopologyId = rxTopology.getTopologyId();
+
+        assertEquals(checkTopologyId.getValue().toString(), mlmtTopologyId.getValue().toString());
+
+        List<Node> lNode = rxTopology.getNode();
+        assertTrue(lNode.isEmpty());
+    }
+
+    private TerminationPointBuilder createTerminationPointBuilder(String nodeName,
+            String tpName) {
+        TpId tpId = new TpId(tpName);
+        TerminationPointKey tpKey = new TerminationPointKey(tpId);
+        TerminationPointBuilder tpBuilder = new TerminationPointBuilder();
+        tpBuilder.setKey(tpKey);
+        tpBuilder.setTpId(tpId);
+        tpBuilder.setTpRef(Collections.<TpId>emptyList());
+
+        return tpBuilder;
+    }
+
+    private void createTp(LogicalDatastoreType type, String nodeTopologyName,
+            String nodeName, String tpName) throws Exception {
+        final InstanceIdentifier<Topology> topologyInstanceId = mlmtTopologyInstanceId;
+        final TopologyId nodeTopologyId = new TopologyId(MLMT1);
+        final TerminationPointBuilder tpBuilder = createTerminationPointBuilder(nodeName, tpName);
+        final NodeKey nodeKey = new NodeKey(new NodeId(nodeName));
+
+        builder.createTp(type, topologyInstanceId, nodeKey, tpBuilder.build());
+
+        synchronized (waitObject) {
+            waitObject.wait(5000);
+        }
+
+        ReadOnlyTransaction rTx = dataBroker.newReadOnlyTransaction();
+        Optional<Topology> optional = rTx.read(type, topologyInstanceId).get();
+        assertNotNull(optional);
+        assertTrue(MLMT1 + " topology not present", optional.isPresent());
+        Topology rxTopology = optional.get();
+        TopologyId checkTopologyId = rxTopology.getTopologyId();
+
+        assertEquals(checkTopologyId.getValue().toString(), mlmtTopologyId.getValue().toString());
+
+        List<Node> lNode = rxTopology.getNode();
+        assertFalse(lNode.isEmpty());
+        /*
+         * building array of node names
+         */
+        List<String> lNodeNames = new ArrayList();
+        List<String> lTpNames = new ArrayList();
+        for (Node cNode : lNode) {
+            String sNodeName = cNode.getNodeId().getValue().toString();
+            lNodeNames.add(sNodeName);
+            if(nodeName.equals(sNodeName)) {
+                List<TerminationPoint> lTp = cNode.getTerminationPoint();
+                assertNotNull(lTp);
+                for (TerminationPoint tp : lTp) {
+                    String cTpName = tp.getKey().getTpId().getValue().toString();
+                    assertNotNull(cTpName);
+                    lTpNames.add(cTpName);
+                }
+                assertTrue(lTpNames.contains(tpName));
+            }
+        }
+
+        assertTrue(lNodeNames.contains(nodeName));
+    }
+
+    private void deleteTp(LogicalDatastoreType type, String nodeTopologyName,
+            String nodeName, String tpName) throws Exception {
+        final InstanceIdentifier<Topology> topologyInstanceId = mlmtTopologyInstanceId;
+        final TopologyId nodeTopologyId = new TopologyId(MLMT1);
+        final NodeKey nodeKey = new NodeKey(new NodeId(nodeName));
+        TpId tpId = new TpId(tpName);
+        TerminationPointKey tpKey = new TerminationPointKey(tpId);
+
+        builder.deleteTp(type, topologyInstanceId, nodeKey, tpKey);
+
+        synchronized (waitObject) {
+            waitObject.wait(5000);
+        }
+
+        ReadOnlyTransaction rTx = dataBroker.newReadOnlyTransaction();
+        Optional<Topology> optional = rTx.read(type, topologyInstanceId).get();
+        assertNotNull(optional);
+        assertTrue(MLMT1 + " topology not present", optional.isPresent());
+        Topology rxTopology = optional.get();
+        TopologyId checkTopologyId = rxTopology.getTopologyId();
+
+        assertEquals(checkTopologyId.getValue().toString(), mlmtTopologyId.getValue().toString());
+
+        List<Node> lNode = rxTopology.getNode();
+        assertFalse(lNode.isEmpty());
+
+        for (Node cNode : lNode) {
+            String sNodeName = cNode.getNodeId().getValue().toString();
+            if (nodeName.equals(sNodeName)) {
+                assertTrue(cNode.getTerminationPoint().isEmpty());
+            }
+        }
+    }
+
+    private String buildLinkName(String sourceNodeName, String sourceTpName, String destNodeName, String destTpName) {
+        return sourceNodeName + "&" + sourceTpName + "&" + destNodeName + "&" + destTpName;
+    }
+
+    private void createLink(LogicalDatastoreType type, String nodeTopologyName,
+            String sourceNodeName, String sourceTpName, String destNodeName, String destTpName) throws Exception {
+        final InstanceIdentifier<Topology> topologyInstanceId = mlmtTopologyInstanceId;
+        final TopologyId nodeTopologyId = new TopologyId(MLMT1);
+
+        LinkBuilder linkBuilder = new LinkBuilder();
+        String sLinkName = buildLinkName(sourceNodeName, sourceTpName, destNodeName, destTpName);
+        LinkId linkId = new LinkId(sLinkName);
+        LinkKey linkKey = new LinkKey(linkId);
+        SourceBuilder sourceBuilder = new SourceBuilder();
+        sourceBuilder.setSourceNode(new NodeId(sourceNodeName));
+        sourceBuilder.setSourceTp(new TpId(sourceTpName));
+        DestinationBuilder destBuilder = new DestinationBuilder();
+        destBuilder.setDestNode(new NodeId(destNodeName));
+        destBuilder.setDestTp(new TpId(destTpName));
+
+        linkBuilder.setKey(linkKey);
+        linkBuilder.setLinkId(linkId);
+        linkBuilder.setSource(sourceBuilder.build());
+        linkBuilder.setDestination(destBuilder.build());
+
+        builder.createLink(type, topologyInstanceId, linkBuilder.build());
+
+        synchronized (waitObject) {
+            waitObject.wait(5000);
+        }
+
+        ReadOnlyTransaction rTx = dataBroker.newReadOnlyTransaction();
+        Optional<Topology> optional = rTx.read(type, topologyInstanceId).get();
+        assertNotNull(optional);
+        assertTrue(MLMT1 + " topology not present", optional.isPresent());
+        Topology rxTopology = optional.get();
+        TopologyId checkTopologyId = rxTopology.getTopologyId();
+
+        assertEquals(checkTopologyId.getValue().toString(), mlmtTopologyId.getValue().toString());
+
+        List<Link> lLink = rxTopology.getLink();
+        assertFalse(lLink.isEmpty());
+
+        List<String> lLinkNames = new ArrayList();
+        for (Link cLink : lLink) {
+            String cLinkName = cLink.getLinkId().getValue().toString();
+            lLinkNames.add(cLinkName);
+        }
+
+        assertTrue(lLinkNames.contains(sLinkName));
+    }
+
+    private void deleteLink(LogicalDatastoreType type, String nodeTopologyName,
+            String sourceNodeName, String sourceTpName, String destNodeName, String destTpName) throws Exception {
+        final InstanceIdentifier<Topology> topologyInstanceId = mlmtTopologyInstanceId;
+        final TopologyId nodeTopologyId = new TopologyId(MLMT1);
+
+        String sLinkName = buildLinkName(sourceNodeName, sourceTpName, destNodeName, destTpName);
+        LinkId linkId = new LinkId(sLinkName);
+        LinkKey linkKey = new LinkKey(linkId);
+
+        builder.deleteLink(type, topologyInstanceId, linkKey);
+
+        synchronized (waitObject) {
+            waitObject.wait(5000);
+        }
+
+        ReadOnlyTransaction rTx = dataBroker.newReadOnlyTransaction();
+        Optional<Topology> optional = rTx.read(type, topologyInstanceId).get();
+        assertNotNull(optional);
+        assertTrue(MLMT1 + " topology not present", optional.isPresent());
+        Topology rxTopology = optional.get();
+        TopologyId checkTopologyId = rxTopology.getTopologyId();
+
+        assertEquals(checkTopologyId.getValue().toString(), mlmtTopologyId.getValue().toString());
+
+        List<Link> lLink = rxTopology.getLink();
+        assertFalse(lLink.isEmpty());
+
+        List<String> lLinkNames = new ArrayList();
+        for (Link cLink : lLink) {
+            String cLinkName = cLink.getLinkId().getValue().toString();
+            lLinkNames.add(cLinkName);
+        }
+
+        assertFalse(lLinkNames.contains(sLinkName));
+    }
+
+    private void deleteLastLink(LogicalDatastoreType type, String nodeTopologyName,
+            String sourceNodeName, String sourceTpName, String destNodeName, String destTpName) throws Exception {
+        final InstanceIdentifier<Topology> topologyInstanceId = mlmtTopologyInstanceId;
+        final TopologyId nodeTopologyId = new TopologyId(MLMT1);
+
+        String sLinkName = buildLinkName(sourceNodeName, sourceTpName, destNodeName, destTpName);
+        LinkId linkId = new LinkId(sLinkName);
+        LinkKey linkKey = new LinkKey(linkId);
+
+        builder.deleteLink(type, topologyInstanceId, linkKey);
+
+        synchronized (waitObject) {
+            waitObject.wait(5000);
+        }
+
+        ReadOnlyTransaction rTx = dataBroker.newReadOnlyTransaction();
+        Optional<Topology> optional = rTx.read(type, topologyInstanceId).get();
+        assertNotNull(optional);
+        assertTrue(MLMT1 + " topology not present", optional.isPresent());
+        Topology rxTopology = optional.get();
+        TopologyId checkTopologyId = rxTopology.getTopologyId();
+
+        assertEquals(checkTopologyId.getValue().toString(), mlmtTopologyId.getValue().toString());
+
+        List<Link> lLink = rxTopology.getLink();
+        assertTrue(lLink.isEmpty());
     }
 
     @Test(timeout = 10000)
@@ -224,9 +513,39 @@ public class MlmtTopologyBuilderTest extends AbstractDataBrokerTest {
     }
 
     @Test(timeout = 10000)
-    public void testCreateNodeConfiguration() throws Exception {
+    public void testCreateNodeTpLinkConfiguration() throws Exception {
         createTopology(LogicalDatastoreType.CONFIGURATION);
-        createNode(LogicalDatastoreType.CONFIGURATION, "testnode");
+        createNode(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:1");
+        createNode(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:2");
+        createTp(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:1", "tp:1");
+        createTp(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:2", "tp:2");
+        createLink(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:1", "tp:1", "node:2", "tp:2");
+        createLink(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:2", "tp:2", "node:1", "tp:1");
+        deleteLink(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:2", "tp:2", "node:1", "tp:1");
+        deleteLastLink(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:1", "tp:1", "node:2", "tp:2");
+        deleteTp(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:2", "tp:2");
+        deleteTp(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:1", "tp:1");
+        deleteNode(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:2");
+        deleteLastNode(LogicalDatastoreType.CONFIGURATION, "example-linkstate", "node:1");
+        deleteTopology(LogicalDatastoreType.CONFIGURATION);
+    }
+
+    @Test(timeout = 10000)
+    public void testCreateNodeTpLinkOperational() throws Exception {
+        createTopology(LogicalDatastoreType.OPERATIONAL);
+        createNode(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:1");
+        createNode(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:2");
+        createTp(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:1", "tp:1");
+        createTp(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:2", "tp:2");
+        createLink(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:1", "tp:1", "node:2", "tp:2");
+        createLink(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:2", "tp:2", "node:1", "tp:1");
+        deleteLink(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:2", "tp:2", "node:1", "tp:1");
+        deleteLastLink(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:1", "tp:1", "node:2", "tp:2");
+        deleteTp(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:2", "tp:2");
+        deleteTp(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:1", "tp:1");
+        deleteNode(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:2");
+        deleteLastNode(LogicalDatastoreType.OPERATIONAL, "example-linkstate", "node:1");
+        deleteTopology(LogicalDatastoreType.OPERATIONAL);
     }
 
     @After
@@ -244,7 +563,5 @@ public class MlmtTopologyBuilderTest extends AbstractDataBrokerTest {
 
     @AfterClass
     public static void allMethodsClear() {
-        // NOOP
     }
 }
-
