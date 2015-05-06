6b9155eca8916638a3a88e77c2ce8c6b72ef8d1f
Martin Uhlir <martin.uhlir@pantheon.sk>
2015-05-06 16:13:50 +0200

    Delegate topology-types from overlay topology request into resulting overlay topology
    Change-Id: I9d2e7d1134b5d9af9ac3f0996c2da9c395eeee2f
    Signed-off-by: Martin Uhlir <martin.uhlir@pantheon.sk>

--- topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/handler/TopologyRequestHandler.java
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/handler/TopologyRequestHandler.java
@@ -42,6 +42,8 @@ import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.InstanceIdentifierBuilder;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -64,6 +66,7 @@ public class TopologyRequestHandler {
     private GlobalSchemaContextHolder schemaHolder;
     private RpcServices rpcServices;
     private DOMTransactionChain transactionChain;
+    private TopologyWriter writer;
 
     /**
      * Default constructor
@@ -97,7 +100,7 @@ public class TopologyRequestHandler {
         Preconditions.checkNotNull(topology, "Received topology can't be null");
         this.topology = topology;
         String overlayTopologyId = topology.getTopologyId().getValue();
-        TopologyWriter writer = new TopologyWriter(overlayTopologyId);
+        writer = new TopologyWriter(overlayTopologyId);
         transactionChain = domDataBroker.createTransactionChain(writer);
         writer.setTransactionChain(transactionChain);
         TopologyManager topologyManager = new TopologyManager(rpcServices, schemaHolder,
@@ -229,5 +232,14 @@ public class TopologyRequestHandler {
         for (ListenerRegistration<DOMDataChangeListener> listener : listeners) {
             listener.close();
         }
+    }
+
+    /**
+     * Delegate topology types to writer
+     * @param topologyTypes - taken from overlay topology request
+     */
+    public void delegateTopologyTypes(
+            DataContainerChild<? extends PathArgument, ?> topologyTypes) {
+        writer.writeTopologyTypes(topologyTypes);
     }
 }
