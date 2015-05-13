9cd063c75638a559484b5ef22b5d2a572a242135
Martin Uhlir <martin.uhlir@pantheon.sk>
2015-05-13 10:35:05 +0200

    Bug 3199 - In correlated topology, supporting-node <topology-ref> and <node-ref> tags contain their respective IDs (instead of full path identifiers)
    Change-Id: Ia82bacb480de332c5d32d6a886554458fb42f659
    Signed-off-by: Martin Uhlir <martin.uhlir@pantheon.sk>
    (cherry picked from commit 4f483703ff41a74acaa559b61b266cce5df8c66e)

--- topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/translator/LogicalNodeToNodeTranslator.java
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/translator/LogicalNodeToNodeTranslator.java
@@ -8,25 +8,31 @@
 
 package org.opendaylight.topoprocessing.impl.translator;
 
-import com.google.common.base.Optional;
-import com.google.common.collect.Iterables;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.opendaylight.topoprocessing.impl.structure.LogicalNode;
 import org.opendaylight.topoprocessing.impl.structure.LogicalNodeWrapper;
 import org.opendaylight.topoprocessing.impl.structure.PhysicalNode;
 import org.opendaylight.topoprocessing.impl.util.TopologyQNames;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.node.attributes.SupportingNode;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.*;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.CollectionNodeBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import com.google.common.base.Optional;
 
 /**
  * @author matus.marko
@@ -56,10 +62,9 @@ public class LogicalNodeToNodeTranslator {
                     writtenNodes.add(physicalNode);
                     NormalizedNode<?, ?> physicalWholeNode = physicalNode.getNode();
                     // prepare supporting nodes
-                    Map<QName, Object> keyValues = new HashMap();
-                    keyValues.put(TopologyQNames.TOPOLOGY_REF, YangInstanceIdentifier.create(Iterables.limit(
-                            physicalNode.getNodeIdentifier().getPathArguments(), 3)).toString());
-                    keyValues.put(TopologyQNames.NODE_REF, physicalNode.getNodeIdentifier().toString());
+                    Map<QName, Object> keyValues = new HashMap<>();
+                    keyValues.put(TopologyQNames.TOPOLOGY_REF, physicalNode.getTopologyId());
+                    keyValues.put(TopologyQNames.NODE_REF, physicalNode.getNodeId());
                     supportingNodes.withChild(ImmutableNodes.mapEntryBuilder().withNodeIdentifier(
                             new YangInstanceIdentifier.NodeIdentifierWithPredicates(
                                     SupportingNode.QNAME, keyValues)).build());
