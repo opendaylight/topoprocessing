8e712c1e6d56186a65477b7be71899e8a2bb3b76
Matus Marko <matus.marko@pantheon.sk>
2015-05-20 10:19:43 +0200

    Bug 3217 - Created TestNodeCreator class for providing nodes for testing purposes
    Change-Id: I14ff8f084c7fd4b14d8bf4049269fc7d1f9d0e4e
    Signed-off-by: Matus Marko <matus.marko@pantheon.sk>
    (cherry picked from commit bd424fe05b117d9ae92b395c3da143717be31c53)

---
+++ topoprocessing-impl/src/test/java/org/opendaylight/topoprocessing/impl/testUtilities/TestNodeCreator.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.topoprocessing.impl.testUtilities;
+
+import org.opendaylight.topoprocessing.impl.util.TopologyQNames;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+
+import static org.opendaylight.topoprocessing.impl.util.InstanceIdentifiers.NODE_IDENTIFIER;
+
+/**
+ * @author matus.marko
+ */
+public class TestNodeCreator {
+
+    private static final QName IP_ADDRESS_QNAME = QName.create(Node.QNAME, "ip-address");
+
+    public YangInstanceIdentifier createNodeIdYiid(String nodeId) {
+        return YangInstanceIdentifier.builder(NODE_IDENTIFIER)
+                .nodeWithKey(Node.QNAME, TopologyQNames.NETWORK_NODE_ID_QNAME, nodeId)
+                .node(TopologyQNames.NETWORK_NODE_ID_QNAME).build();
+    }
+
+    public MapEntryNode createMapEntryNode(String nodeId) {
+        return ImmutableNodes.mapEntryBuilder(Node.QNAME, TopologyQNames.NETWORK_NODE_ID_QNAME, nodeId).build();
+    }
+
+    public MapEntryNode createMapEntryNodeWithIpAddress(String nodeId, String ipAddress) {
+        return ImmutableNodes.mapEntryBuilder(Node.QNAME, TopologyQNames.NETWORK_NODE_ID_QNAME, nodeId)
+                .withChild(ImmutableNodes.leafNode(IP_ADDRESS_QNAME, ipAddress))
+                .build();
+    }
+}
