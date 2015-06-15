2db5a78f757018db7bde1b92509ca41ce3ef9cef
Matus Marko <matus.marko@pantheon.sk>
2015-06-15 10:38:01 +0200

    Bug 3715 - Add Filtrators
    - model changes & filtrator loading will be added soon (next commits)
    
    Change-Id: I8fdc4ee71175fa282a99457d2667adc880cd9a03
    Signed-off-by: Matus Marko <matus.marko@pantheon.sk>
    Signed-off-by: Michal Polkorab <michal.polkorab@pantheon.sk>
    (cherry picked from commit 42ddaf4f9727668390da05c7950847bbb2aeaee9)

---
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/operator/filtrator/RangeNumberFiltrator.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.topoprocessing.impl.operator.filtrator;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import org.opendaylight.topoprocessing.impl.structure.PhysicalNode;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author matus.marko
+ */
+public class RangeNumberFiltrator implements Filtrator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RangeNumberFiltrator.class);
+
+    protected final int min;
+    protected final int max;
+    protected final YangInstanceIdentifier pathIdentifier;
+
+    protected RangeNumberFiltrator(int min, int max, YangInstanceIdentifier pathIdentifier) {
+        Preconditions.checkNotNull(min, "Filtering min value can't be null");
+        Preconditions.checkNotNull(max, "Filtering max value can't be null");
+        Preconditions.checkNotNull(pathIdentifier, "PathIdentifier can't be null");
+        this.min = min;
+        this.max = max;
+        this.pathIdentifier = pathIdentifier;
+    }
+
+    @Override
+    public boolean isFiltered(PhysicalNode node) {
+        Optional<NormalizedNode<?, ?>> leafNode = NormalizedNodes.findNode(node.getNode(), pathIdentifier);
+        if (leafNode.isPresent()) {
+            int value = (int) ((LeafNode) leafNode.get()).getValue();
+            if (this.min <= value && this.max >= value) {
+                return false;
+            }
+        }
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Node with value {} was filtered out", node.getNode());
+        }
+        return true;
+    }
+}
---
+++ topoprocessing-impl/src/test/java/org/opendaylight/topoprocessing/impl/operator/filtrator/RangeStringFiltratorTest.java
@@ -0,0 +1,54 @@
+package org.opendaylight.topoprocessing.impl.operator.filtrator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.topoprocessing.impl.structure.PhysicalNode;
+import org.opendaylight.topoprocessing.impl.util.TopologyQNames;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+
+/**
+ * @author matus.marko
+ */
+public class RangeStringFiltratorTest {
+
+    private static final String TOPOLOGY_ID = "mytopo:1";
+    private static final String NODE_ID = "mynode:1";
+
+    private static final QName ROOT_QNAME = Node.QNAME;
+    private static final QName STRING_QNAME = QName.create(ROOT_QNAME, "string");
+    private final YangInstanceIdentifier path = YangInstanceIdentifier.builder().node(STRING_QNAME).build();
+
+    @Test
+    public void test() {
+        RangeStringFiltrator filtrator = new RangeStringFiltrator("cccc", "hhhh", path);
+
+        boolean filtered1 = filtrator.isFiltered(new PhysicalNode(
+                ImmutableNodes.mapEntryBuilder(Node.QNAME, TopologyQNames.NETWORK_NODE_ID_QNAME, NODE_ID).withChild(
+                        ImmutableNodes.leafNode(STRING_QNAME, "cdef")).build(), null, TOPOLOGY_ID, NODE_ID));
+        Assert.assertFalse("Node should pass the filtrator", filtered1);
+
+        boolean filtered2 = filtrator.isFiltered(new PhysicalNode(
+                ImmutableNodes.mapEntryBuilder(Node.QNAME, TopologyQNames.NETWORK_NODE_ID_QNAME, NODE_ID).withChild(
+                        ImmutableNodes.leafNode(STRING_QNAME, "cccc")).build(), null, TOPOLOGY_ID, NODE_ID));
+        Assert.assertFalse("Node should pass the filtrator", filtered2);
+
+        boolean filtered3 = filtrator.isFiltered(new PhysicalNode(
+                ImmutableNodes.mapEntryBuilder(Node.QNAME, TopologyQNames.NETWORK_NODE_ID_QNAME, NODE_ID).withChild(
+                        ImmutableNodes.leafNode(STRING_QNAME, "hhhh")).build(), null, TOPOLOGY_ID, NODE_ID));
+        Assert.assertFalse("Node should pass the filtrator", filtered3);
+
+        boolean filtered4 = filtrator.isFiltered(new PhysicalNode(
+                ImmutableNodes.mapEntryBuilder(Node.QNAME, TopologyQNames.NETWORK_NODE_ID_QNAME, NODE_ID).withChild(
+                        ImmutableNodes.leafNode(STRING_QNAME, "aaaa")).build(), null, TOPOLOGY_ID, NODE_ID));
+        Assert.assertTrue("Node should not pass the filtrator", filtered4);
+
+        boolean filtered5 = filtrator.isFiltered(new PhysicalNode(
+                ImmutableNodes.mapEntryBuilder(Node.QNAME, TopologyQNames.NETWORK_NODE_ID_QNAME, NODE_ID).withChild(
+                        ImmutableNodes.leafNode(STRING_QNAME, "kkkk")).build(), null, TOPOLOGY_ID, NODE_ID));
+        Assert.assertTrue("Node should not pass the filtrator", filtered5);
+
+    }
+}
