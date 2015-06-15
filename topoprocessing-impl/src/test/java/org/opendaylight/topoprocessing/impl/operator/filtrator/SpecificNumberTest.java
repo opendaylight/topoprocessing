0229db54fb2d2371c1a323bfa9408ab5fb8b8af6
Matus Marko <matus.marko@pantheon.sk>
2015-06-15 10:38:01 +0200

    Bug 3715 - Add Filtrators
    - needs model changes
    
    Change-Id: I8fdc4ee71175fa282a99457d2667adc880cd9a03
    Signed-off-by: Matus Marko <matus.marko@pantheon.sk>

---
+++ topoprocessing-impl/src/test/java/org/opendaylight/topoprocessing/impl/operator/filtrator/SpecificNumberTest.java
@@ -0,0 +1,38 @@
+package org.opendaylight.topoprocessing.impl.operator.filtrator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.topoprocessing.impl.structure.PhysicalNode;
+import org.opendaylight.topoprocessing.impl.util.TopologyQNames;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+
+/**
+ * @author matus.marko
+ */
+public class SpecificNumberTest {
+
+    private static final String TOPOLOGY_ID = "mytopo:1";
+    private static final String NODE_ID = "mynode:1";
+
+    private static final QName ROOT_QNAME = Node.QNAME;
+    private static final QName NUMBER_QNAME = QName.create(ROOT_QNAME, "number");
+    private final YangInstanceIdentifier path = YangInstanceIdentifier.builder().node(NUMBER_QNAME).build();
+
+    @Test
+    public void test() {
+        SpecificNumber filtrator = new SpecificNumber(56, path);
+
+        boolean filtered1 = filtrator.isFiltered(new PhysicalNode(
+                ImmutableNodes.mapEntryBuilder(Node.QNAME, TopologyQNames.NETWORK_NODE_ID_QNAME, NODE_ID).withChild(
+                        ImmutableNodes.leafNode(NUMBER_QNAME, 56)).build(), null, TOPOLOGY_ID, NODE_ID));
+        Assert.assertFalse("Node should pass the filtrator", filtered1);
+
+        boolean filtered2 = filtrator.isFiltered(new PhysicalNode(
+                ImmutableNodes.mapEntryBuilder(Node.QNAME, TopologyQNames.NETWORK_NODE_ID_QNAME, NODE_ID).withChild(
+                        ImmutableNodes.leafNode(NUMBER_QNAME, 16)).build(), null, TOPOLOGY_ID, NODE_ID));
+        Assert.assertTrue("Node should not pass the filtrator", filtered2);
+    }
+}
