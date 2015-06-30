5d83e9cc965f82f28eb07a62eb1da4355f2a02a7
Matus Marko <matus.marko@pantheon.sk>
2015-06-30 15:59:08 +0200

    Bug 3935 - Check for existing data during topoprocessing startup
    Change-Id: Iff77404ed63f7eb8d3cfe5102f7c9098afc03527
    Signed-off-by: Matus Marko <matus.marko@pantheon.sk>
    Signed-off-by: Michal Polkorab <michal.polkorab@pantheon.sk>

--- topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/listener/UnderlayTopologyListener.java
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/listener/UnderlayTopologyListener.java
@@ -8,19 +8,37 @@
 
 package org.opendaylight.topoprocessing.impl.listener;
 
-import java.util.List;
+import com.google.common.base.Preconditions;
 
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import javax.annotation.Nullable;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataChangeListener;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataReadOnlyTransaction;
 import org.opendaylight.topoprocessing.impl.operator.TopologyAggregator;
 import org.opendaylight.topoprocessing.impl.operator.TopologyOperator;
 import org.opendaylight.topoprocessing.impl.structure.PhysicalNode;
+import org.opendaylight.topoprocessing.impl.util.InstanceIdentifiers;
 import org.opendaylight.topoprocessing.impl.util.TopologyQNames;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topoprocessing.provider.impl.rev150209.DatastoreType;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
@@ -31,7 +49,6 @@ import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.google.common.base.Optional;
 
 /**
  * Listens on underlay topology changes
@@ -42,6 +59,10 @@ public class UnderlayTopologyListener implements DOMDataChangeListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(UnderlayTopologyListener.class);
     private static final YangInstanceIdentifier NODE_IDENTIFIER = YangInstanceIdentifier.builder().node(Node.QNAME).build();
     private static final YangInstanceIdentifier NODE_ID_IDENTIFIER = YangInstanceIdentifier.of(TopologyQNames.NETWORK_NODE_ID_QNAME);
+    private final DOMDataBroker domDataBroker;
+    private volatile CountDownLatch lastLatch;
+    private AtomicReferenceFieldUpdater<UnderlayTopologyListener, CountDownLatch> updater =
+            AtomicReferenceFieldUpdater.newUpdater(UnderlayTopologyListener.class, CountDownLatch.class, "lastLatch");
 
     public enum RequestAction {
         CREATE, UPDATE, DELETE
@@ -53,12 +74,14 @@ public class UnderlayTopologyListener implements DOMDataChangeListener {
 
     /**
      * Default constructor
+     * @param domDataBroker DOM Data Broker
      * @param operator processes received notifications (aggregates them)
      * @param underlayTopologyId underlay topology identifier
      * @param pathIdentifier identifies leaf (node), which aggregation / filtering will be based on
      */
-    public UnderlayTopologyListener(TopologyOperator operator, String underlayTopologyId,
+    public UnderlayTopologyListener(DOMDataBroker domDataBroker, TopologyOperator operator, String underlayTopologyId,
             YangInstanceIdentifier pathIdentifier) {
+        this.domDataBroker = domDataBroker;
         this.operator = operator;
         this.underlayTopologyId = underlayTopologyId;
         this.pathIdentifier = pathIdentifier;
@@ -68,6 +91,16 @@ public class UnderlayTopologyListener implements DOMDataChangeListener {
     public void onDataChanged(AsyncDataChangeEvent<YangInstanceIdentifier, NormalizedNode<?, ?>> change) {
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("DataChangeEvent received: {}", change);
+        }
+        CountDownLatch latch = new CountDownLatch(1);
+        CountDownLatch waitLatch = updater.getAndSet(this, latch);
+        Preconditions.checkNotNull(waitLatch, "Read data first");
+        if (0 != waitLatch.getCount()) {
+            try {
+                waitLatch.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Exception while waiting for the latch", e);
+            }
         }
         if (! change.getCreatedData().isEmpty()) {
             LOGGER.debug("Processing createdData");
@@ -82,6 +115,7 @@ public class UnderlayTopologyListener implements DOMDataChangeListener {
             this.proceedDeletionRequest(change.getRemovedPaths());
         }
         LOGGER.debug("DataChangeEvent processed");
+        latch.countDown();
     }
 
     private void proceedChangeRequest(Map<YangInstanceIdentifier, NormalizedNode<?, ?>> map,
@@ -103,7 +137,7 @@ public class UnderlayTopologyListener implements DOMDataChangeListener {
                 } else {
                     throw new IllegalStateException("node-id was not found in: " + entry.getValue());
                 }
-                PhysicalNode physicalNode = null;
+                PhysicalNode physicalNode;
                 if (operator instanceof TopologyAggregator) {
                     // AGGREGATION
                     LOGGER.debug("Finding node: {}", pathIdentifier);
@@ -149,4 +183,56 @@ public class UnderlayTopologyListener implements DOMDataChangeListener {
         operator.processRemovedChanges(identifiers, underlayTopologyId);
     }
 
+    /**
+     * Reads data that were written before overlay topology request was received
+     * @param path path to existing data
+     * @param datastoreType datastore to read from (operational, configuration)
+     */
+    public void readExistingData(YangInstanceIdentifier path, DatastoreType datastoreType) {
+        LOGGER.trace("Reading existing data from dataStore");
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        updater.getAndSet(this, countDownLatch);
+        DOMDataReadOnlyTransaction transaction = domDataBroker.newReadOnlyTransaction();
+        LogicalDatastoreType logicalDatastoreType = (DatastoreType.CONFIGURATION == datastoreType) ?
+                LogicalDatastoreType.CONFIGURATION : LogicalDatastoreType.OPERATIONAL;
+        CheckedFuture<Optional<NormalizedNode<?, ?>>, ReadFailedException> future =
+                transaction.read(logicalDatastoreType, path);
+        Futures.addCallback(future, new FutureCallback<Optional<NormalizedNode<?, ?>>>() {
+            @Override
+            public void onSuccess(Optional<NormalizedNode<?, ?>> result) {
+                if (result.isPresent()) {
+                    LOGGER.debug("Existing data read");
+                    Collection<NormalizedNode<?, ?>> value =
+                            (Collection<NormalizedNode<?, ?>>) result.get().getValue();
+                    proceedChangeRequest(listToMap(value.iterator(), underlayTopologyId), RequestAction.CREATE);
+                } else {
+                    LOGGER.debug("No data present. Proceeding with notifications.");
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                LOGGER.warn("DataStore read failed - no existing data were read");
+            }
+        });
+        countDownLatch.countDown();
+    }
+
+    private Map<YangInstanceIdentifier, NormalizedNode<?, ?>> listToMap(
+            Iterator<NormalizedNode<?, ?>> nodes, final String underlayTopologyId) {
+        Map<YangInstanceIdentifier, NormalizedNode<?, ?>> map = Maps.uniqueIndex(nodes,
+                new Function<NormalizedNode<?, ?>, YangInstanceIdentifier>() {
+            @Nullable
+            @Override
+            public YangInstanceIdentifier apply(NormalizedNode<?, ?> node) {
+                return YangInstanceIdentifier
+                        .builder(InstanceIdentifiers.TOPOLOGY_IDENTIFIER)
+                        .nodeWithKey(Topology.QNAME, TopologyQNames.TOPOLOGY_ID_QNAME, underlayTopologyId)
+                        .node(Node.QNAME)
+                        .node(node.getIdentifier())
+                        .build();
+            }
+        });
+        return map;
+    }
 }
