e752b323b6814b1e8fabb7f42123a430f2e4f432
Andrej Záň <andrej.zan@pantheon.sk>
2015-08-19 16:56:04 +0200

    Created configuration for topoprocessing i2rs model module
    Change-Id: Ic8847eaa50f08eadc5d99c9a70b2dbcebf24219a
    Signed-off-by: Andrej Záň <andrej.zan@pantheon.sk>

---
+++ topoprocessing-i2rs/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/topoprocessing/i2rs/provider/impl/rev150819/TopoProcessingI2rsProviderModule.java
@@ -0,0 +1,29 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topoprocessing.i2rs.provider.impl.rev150819;
+
+import org.opendaylight.topoprocessing.i2rs.TopoProcessingI2rsProvider;
+import org.opendaylight.topoprocessing.spi.provider.TopoProcessingProvider;
+
+public class TopoProcessingI2rsProviderModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topoprocessing.i2rs.provider.impl.rev150819.AbstractTopoProcessingI2rsProviderModule {
+
+    public TopoProcessingI2rsProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public TopoProcessingI2rsProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topoprocessing.i2rs.provider.impl.rev150819.TopoProcessingI2rsProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        TopoProcessingProvider topoProvider = getTopoprocessingProviderDependency();
+        TopoProcessingI2rsProvider i2rsProvider = new TopoProcessingI2rsProvider();
+        i2rsProvider.startup(topoProvider);
+        return i2rsProvider;
+    }
+
+}
