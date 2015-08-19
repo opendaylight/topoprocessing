b7b639939e2aec633bfc8cf7d131eb2337c39a32
Andrej Záň <andrej.zan@pantheon.sk>
2015-08-19 15:15:38 +0200

    Created configuration for topoprocessing network model module
    Change-Id: I4a7dc97acbc69bab58cad36c3d0d703e76b1c809
    Signed-off-by: Andrej Záň <andrej.zan@pantheon.sk>

---
+++ topoprocessing-nt/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/topoprocessing/nt/provider/impl/rev150819/TopoProcessingNTProviderModule.java
@@ -0,0 +1,29 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topoprocessing.nt.provider.impl.rev150819;
+
+import org.opendaylight.topoprocessing.nt.TopoProcessingNTProvider;
+import org.opendaylight.topoprocessing.spi.provider.TopoProcessingProvider;
+
+public class TopoProcessingNTProviderModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topoprocessing.nt.provider.impl.rev150819.AbstractTopoProcessingNTProviderModule {
+
+    public TopoProcessingNTProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public TopoProcessingNTProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topoprocessing.nt.provider.impl.rev150819.TopoProcessingNTProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        TopoProcessingProvider topoProvider = getTopoprocessingProviderDependency();
+        TopoProcessingNTProvider ntProvider = new TopoProcessingNTProvider();
+        ntProvider.startup(topoProvider);
+        return ntProvider;
+    }
+
+}
