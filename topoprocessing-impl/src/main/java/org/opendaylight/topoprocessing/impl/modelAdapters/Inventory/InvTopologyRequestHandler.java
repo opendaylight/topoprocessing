d3c862ec20230a95c0f840c90baea406ca8a190b
Matej Perina <matej.perina@pantheon.sk>
2015-08-24 11:35:54 +0200

    Model specific approach. Inventory model
    Change-Id: Icf1ef0a4f030368253119df26c7650e024539f40
    Signed-off-by: Matej Perina <matej.perina@pantheon.sk>

---
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/modelAdapters/Inventory/InvTopologyRequestHandler.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.topoprocessing.impl.modelAdapters.Inventory;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.topoprocessing.impl.request.TopologyRequestHandler;
+import org.opendaylight.topoprocessing.impl.rpc.RpcServices;
+import org.opendaylight.topoprocessing.impl.util.GlobalSchemaContextHolder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.CorrelationAugment;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.Model;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.correlations.grouping.Correlations;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * @author matej.perina
+ *
+ */
+public class InvTopologyRequestHandler extends TopologyRequestHandler{
+
+
+    public InvTopologyRequestHandler(DOMDataBroker domDataBroker, GlobalSchemaContextHolder schemaHolder,
+            RpcServices rpcServices, Map.Entry<InstanceIdentifier<?>,DataObject> fromNormalizedNode) {
+        super(domDataBroker, schemaHolder, rpcServices, fromNormalizedNode);
+    }
+
+    @Override
+    protected String getTopologyId(Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode) {
+        return ((Topology) fromNormalizedNode.getValue()).getTopologyId().getValue();
+    }
+
+    @Override
+    protected Correlations getCorrelations(Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode) {
+        return ((Topology) fromNormalizedNode.getValue()).getAugmentation(CorrelationAugment.class).getCorrelations();
+    }
+
+    @Override
+    protected Model getModel(Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode) {
+        return ((Topology) fromNormalizedNode.getValue()).getAugmentation(CorrelationAugment.class)
+                .getCorrelations().getOutputModel();
+    }
+
+}
