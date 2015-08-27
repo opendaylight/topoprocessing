3fc86bee4c5eee3c6a2e27b097d066794e431498
Matej Perina <matej.perina@pantheon.sk>
2015-08-27 13:57:27 +0200

    Model specific approach. Inventory model
    Cointains all classes required for work with Inventory model.
    
    Change-Id: Icf1ef0a4f030368253119df26c7650e024539f40
    Signed-off-by: Matej Perina <matej.perina@pantheon.sk>

---
+++ topoprocessing-inventory/src/main/java/org/opendaylight/topoprocessing/inventory/adapter/InvModelAdapter.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.topoprocessing.inventory.adapter;
+
+import java.util.List;
+import java.util.Map;
+
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataChangeListener;
+import org.opendaylight.topoprocessing.impl.adapter.ModelAdapter;
+import org.opendaylight.topoprocessing.impl.listener.UnderlayTopologyListener;
+import org.opendaylight.topoprocessing.impl.operator.TopologyOperator;
+import org.opendaylight.topoprocessing.impl.request.TopologyRequestListener;
+import org.opendaylight.topoprocessing.impl.rpc.RpcServices;
+import org.opendaylight.topoprocessing.impl.translator.OverlayItemTranslator;
+import org.opendaylight.topoprocessing.impl.util.GlobalSchemaContextHolder;
+import org.opendaylight.topoprocessing.inventory.listener.InvUnderlayTopologyListener;
+import org.opendaylight.topoprocessing.inventory.request.InvTopologyRequestListener;
+import org.opendaylight.topoprocessing.inventory.translator.InvLinkTranslator;
+import org.opendaylight.topoprocessing.inventory.translator.InvNodeTranslator;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topoprocessing.provider.impl.rev150209.DatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.CorrelationItemEnum;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.Model;
+import org.opendaylight.yangtools.binding.data.codec.api.BindingNormalizedNodeSerializer;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+
+/** *
+ * @author matej.perina
+ *
+ */
+public class InvModelAdapter implements ModelAdapter {
+
+    @Override
+    public UnderlayTopologyListener registerUnderlayTopologyListener(DOMDataBroker domDataBroker,
+            String underlayTopologyId, CorrelationItemEnum correlationItem, DatastoreType datastoreType, TopologyOperator operator
+            ,List<ListenerRegistration<DOMDataChangeListener>> listeners) {
+
+        InvUnderlayTopologyListener listener = new InvUnderlayTopologyListener(domDataBroker, underlayTopologyId, correlationItem);
+        listener.registerUnderlayTopologyListener(datastoreType,operator,listeners);
+        return listener;
+    }
+
+    @Override
+    public TopologyRequestListener createTopologyRequestListener(DOMDataBroker dataBroker,
+            BindingNormalizedNodeSerializer nodeSerializer, GlobalSchemaContextHolder schemaHolder,
+            RpcServices rpcServices, Map<Model, ModelAdapter> modelAdapters) {
+        return new InvTopologyRequestListener(dataBroker, nodeSerializer, schemaHolder, rpcServices, modelAdapters);
+    }
+
+    @Override
+    public OverlayItemTranslator createOverlayItemTranslator() {
+        return new OverlayItemTranslator(new InvNodeTranslator(), new InvLinkTranslator());
+    }
+
+}
--- topoprocessing-inventory/src/main/java/org/opendaylight/topoprocessing/inventory/provider/TopoProcessingProviderInv.java
+++ topoprocessing-inventory/src/main/java/org/opendaylight/topoprocessing/inventory/provider/TopoProcessingProviderInv.java
@@ -6,15 +6,17 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.topoprocessing.inventory;
+package org.opendaylight.topoprocessing.inventory.provider;
 
+import org.opendaylight.topoprocessing.inventory.adapter.InvModelAdapter;
 import org.opendaylight.topoprocessing.spi.provider.TopoProcessingProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TopoProcessingInventoryProvider implements AutoCloseable {
+public class TopoProcessingProviderInv implements AutoCloseable {
     private static final Logger LOGGER = LoggerFactory
-            .getLogger(TopoProcessingInventoryProvider.class);
+            .getLogger(TopoProcessingProviderInv.class);
 
     @Override
     public void close() throws Exception {
@@ -23,5 +25,6 @@ public class TopoProcessingInventoryProvider implements AutoCloseable {
 
     public void startup(TopoProcessingProvider topoProvider) {
         LOGGER.info("TopoprocessingNTProvider startup");
+        topoProvider.registerModelAdapter(Model.OpendaylightInventory, new InvModelAdapter());
     }
 }
