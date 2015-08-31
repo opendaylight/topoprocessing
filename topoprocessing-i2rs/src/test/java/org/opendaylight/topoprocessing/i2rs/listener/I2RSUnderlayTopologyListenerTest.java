d0851fe051d6dee36c855bd87f25e66289959bf0
Andrej Záň <andrej.zan@pantheon.sk>
2015-08-31 10:57:19 +0200

    Create unittests for I2RS support implementation
    Change-Id: I27e94d33f26894188a46e4288c255ecafcfa6ade
    Signed-off-by: Andrej Záň <andrej.zan@pantheon.sk>

---
+++ topoprocessing-i2rs/src/test/java/org/opendaylight/topoprocessing/i2rs/listener/I2RSUnderlayTopologyListenerTest.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.topoprocessing.i2rs.listener;
+
+import com.google.common.base.Optional;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.controller.md.sal.dom.broker.impl.PingPongDataBroker;
+import org.opendaylight.topoprocessing.api.structure.UnderlayItem;
+import org.opendaylight.topoprocessing.impl.listener.UnderlayTopologyListener;
+import org.opendaylight.topoprocessing.impl.operator.TopologyAggregator;
+import org.opendaylight.topoprocessing.impl.operator.TopologyFiltrator;
+import org.opendaylight.topoprocessing.impl.testUtilities.TestDataTreeCandidateNode;
+import org.opendaylight.topoprocessing.impl.testUtilities.TestingDOMDataBroker;
+import org.opendaylight.topoprocessing.impl.util.TopologyQNames;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.network.rev150608.network.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.CorrelationItemEnum;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.ModificationType;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+
+/**
+ * @author andrej.zan
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class I2RSUnderlayTopologyListenerTest {
+
+    private static final String TOPOLOGY_ID = "mytopo:1";
+
+    @Mock private Collection<DataTreeCandidate> mockCollection;
+    @Mock private Iterator<DataTreeCandidate> mockIteratorCandidate;
+    @Mock private DataTreeCandidate mockDataTreeCandidate;
+    @Mock private DataTreeCandidateNode mockDataTreeCandidateNode;
+    @Mock private Collection<DataTreeCandidateNode> mockDataTreeCandidateNodeCollection;
+    @Mock private Iterator<DataTreeCandidateNode> mockDataTreeCandidateNodeIterator;
+
+    private PingPongDataBroker dataBroker;
+    private TestingDOMDataBroker testingDOMDataBroker;
+
+    @Before
+    public void setUp() {
+        testingDOMDataBroker = new TestingDOMDataBroker();
+        dataBroker = new PingPongDataBroker(testingDOMDataBroker);
+    }
+
+    @Test
+    public void testAggregation() {
+        QName ipAddressQname = QName.create(Node.QNAME, "ip-address");
+        String nodeName = "node:1";
+        String ipAddress = "192.168.1.1";
+        YangInstanceIdentifier nodeYiid = YangInstanceIdentifier.builder()
+                .node(Node.QNAME)
+                .nodeWithKey(Node.QNAME, TopologyQNames.I2RS_NODE_ID_QNAME, nodeName)
+                .build();
+
+        LeafNode<String> nodeIpValue = ImmutableNodes.leafNode(ipAddressQname, ipAddress);
+        MapEntryNode nodeValueWithIp = ImmutableNodes.mapEntryBuilder(
+                Node.QNAME, TopologyQNames.I2RS_NODE_ID_QNAME, nodeName).addChild(nodeIpValue).build();
+
+        YangInstanceIdentifier pathIdentifier = YangInstanceIdentifier.of(ipAddressQname);
+        TopologyAggregator mockOperator = Mockito.mock(TopologyAggregator.class);
+        UnderlayTopologyListener listener = new I2RSUnderlayTopologyListener(dataBroker, TOPOLOGY_ID,
+                CorrelationItemEnum.Node);
+        listener.setOperator(mockOperator);
+        listener.setPathIdentifier(pathIdentifier);
+
+        UnderlayItem physicalNode = new UnderlayItem(nodeValueWithIp, nodeIpValue,
+                TOPOLOGY_ID, nodeName, CorrelationItemEnum.Node);
+        NodeIdentifierWithPredicates nodePathArgument =
+                new NodeIdentifierWithPredicates(Node.QNAME, TopologyQNames.I2RS_NODE_ID_QNAME, nodeName);
+        TestDataTreeCandidateNode rootNode = new TestDataTreeCandidateNode();
+
+        // create
+        setUpMocks(rootNode);
+        rootNode.setModificationType(ModificationType.WRITE);
+        Optional<NormalizedNode<?, ?>> dataAfter = Optional.<NormalizedNode<?, ?>>fromNullable(nodeValueWithIp);
+        rootNode.setDataAfter(dataAfter);
+        rootNode.setIdentifier(nodePathArgument);
+        listener.onDataTreeChanged(mockCollection);
+        Mockito.verify(mockOperator).processCreatedChanges(Matchers.eq(nodeYiid), Matchers.refEq(physicalNode),
+                Matchers.eq(TOPOLOGY_ID));
+
+        // update
+        resetMocks();
+        setUpMocks(rootNode);
+        rootNode.setModificationType(ModificationType.SUBTREE_MODIFIED);
+        listener.onDataTreeChanged(mockCollection);
+        Mockito.verify(mockOperator).processUpdatedChanges(Matchers.eq(nodeYiid), Matchers.refEq(physicalNode),
+                Matchers.eq(TOPOLOGY_ID));
+
+        // delete
+        resetMocks();
+        setUpMocks(rootNode);
+        rootNode.setModificationType(ModificationType.DELETE);
+        rootNode.setIdentifier(nodePathArgument);
+        YangInstanceIdentifier nodeIdYiid = YangInstanceIdentifier.builder()
+                .node(Node.QNAME).nodeWithKey(Node.QNAME, TopologyQNames.I2RS_NODE_ID_QNAME, nodeName).build();
+        listener.onDataTreeChanged(mockCollection);
+        ArrayList<YangInstanceIdentifier> identifiers = new ArrayList<>();
+        identifiers.add(nodeIdYiid);
+        Mockito.verify(mockOperator).processRemovedChanges(Matchers.eq(nodeYiid), Matchers.eq(TOPOLOGY_ID));
+    }
+
+    private void setUpMocks(TestDataTreeCandidateNode rootNode) {
+        Mockito.when(mockCollection.iterator()).thenReturn(mockIteratorCandidate);
+        Mockito.when(mockIteratorCandidate.hasNext()).thenReturn(true, false);
+        Mockito.when(mockIteratorCandidate.next()).thenReturn(mockDataTreeCandidate);
+        Mockito.when(mockDataTreeCandidate.getRootNode()).thenReturn(mockDataTreeCandidateNode);
+        Mockito.when(mockDataTreeCandidateNode.getChildNodes()).thenReturn(mockDataTreeCandidateNodeCollection);
+        Mockito.when(mockDataTreeCandidateNodeCollection.iterator()).thenReturn(mockDataTreeCandidateNodeIterator);
+        Mockito.when(mockDataTreeCandidateNodeIterator.hasNext()).thenReturn(true, false);
+        Mockito.when(mockDataTreeCandidateNodeIterator.next()).thenReturn(rootNode);
+    }
+
+    private void resetMocks() {
+        Mockito.reset(mockCollection, mockIteratorCandidate, mockDataTreeCandidate, mockDataTreeCandidateNode,
+                mockDataTreeCandidateNodeCollection, mockDataTreeCandidateNodeIterator);
+    }
+
+    @Test
+    public void testFiltrationRequest() {
+        String nodeName = "node:1";
+        YangInstanceIdentifier nodeYiid = YangInstanceIdentifier.builder()
+                .node(Node.QNAME)
+                .nodeWithKey(Node.QNAME, TopologyQNames.I2RS_NODE_ID_QNAME, nodeName)
+                .build();
+        MapEntryNode nodeValue = ImmutableNodes.mapEntry(Node.QNAME, TopologyQNames.I2RS_NODE_ID_QNAME, nodeName);
+
+        TopologyFiltrator mockOperator = Mockito.mock(TopologyFiltrator.class);
+        UnderlayTopologyListener listener = new I2RSUnderlayTopologyListener(dataBroker, TOPOLOGY_ID,
+                CorrelationItemEnum.Node);
+        listener.setOperator(mockOperator);
+        UnderlayItem physicalNode = new UnderlayItem(nodeValue, null, TOPOLOGY_ID, nodeName, CorrelationItemEnum.Node);
+
+        TestDataTreeCandidateNode rootNode = new TestDataTreeCandidateNode();
+        NodeIdentifierWithPredicates nodePathArgument =
+                new NodeIdentifierWithPredicates(Node.QNAME, TopologyQNames.I2RS_NODE_ID_QNAME, nodeName);
+
+        // create
+        setUpMocks(rootNode);
+        rootNode.setModificationType(ModificationType.WRITE);
+        Optional<NormalizedNode<?, ?>> dataAfter = Optional.<NormalizedNode<?, ?>>fromNullable(nodeValue);
+        rootNode.setDataAfter(dataAfter);
+        rootNode.setIdentifier(nodePathArgument);
+        listener.onDataTreeChanged(mockCollection);
+        Mockito.verify(mockOperator).processCreatedChanges(Matchers.eq(nodeYiid), Matchers.refEq(physicalNode),
+                Matchers.eq(TOPOLOGY_ID));
+
+        // update
+        resetMocks();
+        setUpMocks(rootNode);
+        rootNode.setModificationType(ModificationType.SUBTREE_MODIFIED);
+        listener.onDataTreeChanged(mockCollection);
+        Mockito.verify(mockOperator).processUpdatedChanges(Matchers.eq(nodeYiid), Matchers.refEq(physicalNode),
+                Matchers.eq(TOPOLOGY_ID));
+
+        // delete
+        resetMocks();
+        setUpMocks(rootNode);
+        rootNode.setModificationType(ModificationType.DELETE);
+        YangInstanceIdentifier nodeIdYiid = YangInstanceIdentifier.builder()
+                .node(Node.QNAME).nodeWithKey(Node.QNAME, TopologyQNames.I2RS_NODE_ID_QNAME, nodeName).build();
+        listener.onDataTreeChanged(mockCollection);
+        Mockito.verify(mockOperator).processRemovedChanges(Matchers.eq(nodeIdYiid), Matchers.eq(TOPOLOGY_ID));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testIncorrectNode() {
+        String topoId = "node:1";
+        String nodeName = "node:1";
+        YangInstanceIdentifier nodeYiid = YangInstanceIdentifier.of(Node.QNAME);
+        MapEntryNode nodeValue = ImmutableNodes.mapEntry(Node.QNAME, TopologyQNames.I2RS_NETWORK_ID_QNAME, topoId);
+
+        TopologyAggregator mockOperator = Mockito.mock(TopologyAggregator.class);
+        UnderlayTopologyListener listener = new I2RSUnderlayTopologyListener(dataBroker, TOPOLOGY_ID,
+                CorrelationItemEnum.Node);
+        listener.setOperator(mockOperator);
+        listener.setPathIdentifier(nodeYiid);
+        TestDataTreeCandidateNode rootNode = new TestDataTreeCandidateNode();
+        NodeIdentifierWithPredicates nodePathArgument =
+                new NodeIdentifierWithPredicates(Node.QNAME, TopologyQNames.I2RS_NODE_ID_QNAME, nodeName);
+
+        setUpMocks(rootNode);
+        rootNode.setModificationType(ModificationType.WRITE);
+        Optional<NormalizedNode<?, ?>> dataAfter = Optional.<NormalizedNode<?, ?>>fromNullable(nodeValue);
+        rootNode.setDataAfter(dataAfter);
+        rootNode.setIdentifier(nodePathArgument);
+        listener.onDataTreeChanged(mockCollection);
+    }
+}
---
+++ topoprocessing-i2rs/src/test/java/org/opendaylight/topoprocessing/i2rs/provider/I2RSTopoProcessingProviderTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2015 Pantheon Technologies s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.topoprocessing.i2rs.provider;
+
+
+import java.net.URI;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataChangeListener;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcProviderService;
+import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
+import org.opendaylight.controller.md.sal.dom.broker.impl.PingPongDataBroker;
+import org.opendaylight.controller.sal.core.api.model.SchemaService;
+import org.opendaylight.topoprocessing.i2rs.TopoProcessingProviderI2RS;
+import org.opendaylight.topoprocessing.impl.provider.TopoProcessingProviderImpl;
+import org.opendaylight.topoprocessing.impl.request.TopologyRequestListener;
+import org.opendaylight.topoprocessing.impl.rpc.RpcServices;
+import org.opendaylight.topoprocessing.impl.util.InstanceIdentifiers;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topoprocessing.provider.impl.rev150209.DatastoreType;
+import org.opendaylight.yangtools.binding.data.codec.api.BindingNormalizedNodeSerializer;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.model.api.Module;
+import org.opendaylight.yangtools.yang.model.api.ModuleIdentifier;
+import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
+import org.opendaylight.yangtools.yang.model.util.AbstractSchemaContext;
+
+import com.google.common.collect.SetMultimap;
+
+/**
+ * @author andrej.zan
+ *
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class I2RSTopoProcessingProviderTest {
+
+    private TopoProcessingProviderImpl topoProcessingProvider;
+
+    @Mock private SchemaService schemaService;
+    @Mock private DOMDataBroker dataBroker;
+    @Mock private BindingNormalizedNodeSerializer nodeSerializer;
+    @Mock private RpcServices rpcServices;
+    @Mock private ListenerRegistration<SchemaContextListener> schemaContextListenerRegistration;
+    @Mock private ListenerRegistration<DOMDataChangeListener> topologyRequestListenerRegistration;
+
+    private PingPongDataBroker pingPongDataBroker;
+
+    private class SchemaContextTmp extends AbstractSchemaContext {
+
+        @Override
+        protected Map<ModuleIdentifier, String> getIdentifiersToSources() {
+            return null;
+        }
+
+        @Override
+        protected SetMultimap<URI, Module> getNamespaceToModules() {
+            return null;
+        }
+
+        @Override
+        protected SetMultimap<String, Module> getNameToModules() {
+            return null;
+        }
+
+        @Override
+        public Set<Module> getModules() {
+            return null;
+        }
+    }
+
+    @Test
+    public void testStartup() throws Exception {
+        pingPongDataBroker = new PingPongDataBroker(dataBroker);
+        Mockito.when(rpcServices.getRpcService()).thenReturn(Mockito.mock(DOMRpcService.class));
+        Mockito.when(rpcServices.getRpcProviderService()).thenReturn(Mockito.mock(DOMRpcProviderService.class));
+        SchemaContextTmp schemaContext = new SchemaContextTmp();
+        Mockito.when(schemaService.getGlobalContext()).thenReturn(schemaContext);
+        Mockito.when(schemaService.registerSchemaContextListener((SchemaContextListener) Matchers.any()))
+                .thenReturn(schemaContextListenerRegistration);
+        Mockito.when(dataBroker.registerDataChangeListener((LogicalDatastoreType) Matchers.any(),
+                (YangInstanceIdentifier) Matchers.any(), (DOMDataChangeListener) Matchers.any(),
+                (DataChangeScope) Matchers.any())).thenReturn(topologyRequestListenerRegistration);
+
+        // startup
+        topoProcessingProvider = new TopoProcessingProviderImpl(
+                schemaService, pingPongDataBroker, nodeSerializer, rpcServices, DatastoreType.OPERATIONAL);
+        topoProcessingProvider.startup();
+        TopoProcessingProviderI2RS I2RSProvider = new TopoProcessingProviderI2RS();
+        I2RSProvider.startup(topoProcessingProvider);
+        Mockito.verify(schemaService).registerSchemaContextListener((SchemaContextListener) Matchers.any());
+        Mockito.verify(dataBroker).registerDataChangeListener(
+                Matchers.eq(LogicalDatastoreType.CONFIGURATION),
+                Matchers.eq(InstanceIdentifiers.TOPOLOGY_IDENTIFIER),
+                Matchers.any(TopologyRequestListener.class),
+                Matchers.eq(DataChangeScope.ONE));
+
+        // close
+        topoProcessingProvider.close();
+        I2RSProvider.close();
+        Mockito.verify(schemaContextListenerRegistration).close();
+        Mockito.verify(topologyRequestListenerRegistration).close();
+    }
+}
