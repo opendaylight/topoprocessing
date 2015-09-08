ea990c35bdc7c5c8125928afb9bf9f4e10cab9ca
giorgiogarziano <giorgio.garziano@ericsson.com>
2015-09-08 17:12:50 +0200

    Improved mlmt multilayer topology provider junit tests
    Change-Id: I508406ef691f4a17431a55858653bdfdddde90f2
    Signed-off-by: giorgiogarziano <giorgio.garziano@ericsson.com>

--- mlmt/multilayer-topology/impl/multilayer/src/test/java/org/opendaylight/topology/multilayer/MultilayerTopologyProviderTest.java
+++ mlmt/multilayer-topology/impl/multilayer/src/test/java/org/opendaylight/topology/multilayer/MultilayerTopologyProviderTest.java
@@ -10,6 +10,7 @@ package org.opendaylight.topology.multilayer;
 
 import com.google.common.base.Optional;
 
+import java.util.concurrent.Future;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -75,6 +76,34 @@ import org.opendaylight.topology.mlmt.utility.MlmtOperationProcessor;
 import org.opendaylight.topology.multilayer.MultilayerTopologyProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.ted.rev150122.NativeL3IgpMetric;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.ted.rev150122.MtLinkMetricAttributeValue;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.ted.rev150122.MtLinkMetricAttributeValueBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.rev150122.mt.info.attribute.ValueBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.rev150122.mt.info.AttributeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.rev150122.mt.info.Attribute;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.rev150122.mt.info.AttributeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.NetworkTopologyRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.ForwardingAdjAnnounceInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.ForwardingAdjAnnounceInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.ForwardingAdjAnnounceOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.ForwardingAdjUpdateInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.ForwardingAdjUpdateOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.ForwardingAdjWithdrawInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.ForwardingAdjWithdrawInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.ForwardingAdjWithdrawOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.forwarding.adjacency.attributes.HeadEnd;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.forwarding.adjacency.attributes.TailEnd;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.FaEndPoint;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.fa.end.point.StitchingPoint;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.forwarding.adjacency.attributes.AnnouncementContextBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.forwarding.adjacency.attributes.HeadEndBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.forwarding.adjacency.attributes.TailEndBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.fa.parameters.directionality.info.unidirectional.UnidirectionalBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.forwarding.adj.announce.output.result.FaId;
+
+import org.opendaylight.yangtools.yang.common.RpcResult;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MultilayerTopologyProviderTest extends AbstractDataBrokerTest {
@@ -226,7 +255,7 @@ public class MultilayerTopologyProviderTest extends AbstractDataBrokerTest {
     }
 
     @Test(timeout = 10000)
-    public void onTopologyCreatedTest() throws Exception {
+    public void testOnTopologyCreated() throws Exception {
         provider.onTopologyCreated(LogicalDatastoreType.OPERATIONAL, exampleIid, exampleTopology);
 
         synchronized (waitObject) {
@@ -249,7 +278,7 @@ public class MultilayerTopologyProviderTest extends AbstractDataBrokerTest {
     }
 
     @Test(timeout = 10000)
-    public void onNodeCreatedTest() throws Exception {
+    public void testOnNodeCreated() throws Exception {
         NodeBuilder nodeBuilder = new NodeBuilder();
         String nodeName = "node:1";
         NodeId nodeId = new NodeId(nodeName);
@@ -273,7 +302,7 @@ public class MultilayerTopologyProviderTest extends AbstractDataBrokerTest {
     }
 
     @Test(timeout = 10000)
-    public void onTpCreatedTest() throws Exception {
+    public void testOnTpCreated() throws Exception {
         NodeBuilder nodeBuilder = new NodeBuilder();
         String nodeName = "node:1";
         NodeId nodeId = new NodeId(nodeName);
@@ -303,7 +332,7 @@ public class MultilayerTopologyProviderTest extends AbstractDataBrokerTest {
     }
 
     @Test(timeout = 10000)
-    public void onLinkCreatedTest() throws Exception {
+    public void testOnLinkCreated() throws Exception {
         LinkBuilder linkBuilder = new LinkBuilder();
         String linkName = "link:1";
         LinkId linkId = new LinkId(linkName);
@@ -324,6 +353,156 @@ public class MultilayerTopologyProviderTest extends AbstractDataBrokerTest {
         assertTrue("Operational mlmt:1 topology ", optional.isPresent());
         Topology rxTopology = optional.get();
         assertNotNull(rxTopology);
+    }
+
+    @Test(timeout = 10000)
+    public void testOnFaAnnounce() throws Exception {
+        InstanceIdentifier<Topology> topologyIid = buildTopologyIid(MLMT);
+        final Topology wrTopology = buildMlmtTopology(MLMT);
+        WriteTransaction rwTx = dataBroker.newWriteOnlyTransaction();
+        rwTx.merge(LogicalDatastoreType.OPERATIONAL, topologyIid, wrTopology, true);
+        assertCommit(rwTx.submit());
+
+        synchronized (waitObject) {
+            waitObject.wait(1500);
+        }
+
+        String nodeName1 = "node:1";
+        NodeBuilder nodeBuilder = new NodeBuilder();
+        NodeId nodeId1 = new NodeId(nodeName1);
+        NodeKey nodeKey = new NodeKey(nodeId1);
+        nodeBuilder.setKey(nodeKey);
+        nodeBuilder.setNodeId(nodeId1);
+        Node wrNode = nodeBuilder.build();
+        InstanceIdentifier<Node> nodeIid = topologyIid.child(Node.class, nodeKey);
+        rwTx = dataBroker.newWriteOnlyTransaction();
+        rwTx.merge(LogicalDatastoreType.OPERATIONAL, nodeIid, wrNode);
+        assertCommit(rwTx.submit());
+
+        synchronized (waitObject) {
+             waitObject.wait(1500);
+        }
+
+        TerminationPointBuilder tpBuilder = new TerminationPointBuilder();
+        String tpName1 = "1:1";
+        TpId tpId1 = new TpId(tpName1);
+        TerminationPointKey tpKey = new TerminationPointKey(tpId1);
+        tpBuilder.setKey(tpKey);
+        tpBuilder.setTpId(tpId1);
+        TerminationPoint tp = tpBuilder.build();
+        InstanceIdentifier<TerminationPoint> tpIid = nodeIid.child(TerminationPoint.class, tpKey);
+        rwTx = dataBroker.newWriteOnlyTransaction();
+        rwTx.merge(LogicalDatastoreType.OPERATIONAL, tpIid, tp);
+        assertCommit(rwTx.submit());
+
+        synchronized (waitObject) {
+             waitObject.wait(1500);
+        }
+
+        String nodeName2 = "node:2";
+        nodeBuilder = new NodeBuilder();
+        NodeId nodeId2 = new NodeId(nodeName2);
+        nodeKey = new NodeKey(nodeId2);
+        nodeBuilder.setKey(nodeKey);
+        nodeBuilder.setNodeId(nodeId2);
+        wrNode = nodeBuilder.build();
+        nodeIid = topologyIid.child(Node.class, nodeKey);
+        rwTx = dataBroker.newWriteOnlyTransaction();
+        rwTx.merge(LogicalDatastoreType.OPERATIONAL, nodeIid, wrNode);
+        assertCommit(rwTx.submit());
+
+        synchronized (waitObject) {
+             waitObject.wait(1500);
+        }
+
+        tpBuilder = new TerminationPointBuilder();
+        String tpName2 = "2:1";
+        TpId tpId2 = new TpId(tpName2);
+        tpKey = new TerminationPointKey(tpId2);
+        tpBuilder.setKey(tpKey);
+        tpBuilder.setTpId(tpId2);
+        tp = tpBuilder.build();
+        tpIid = nodeIid.child(TerminationPoint.class, tpKey);
+        rwTx = dataBroker.newWriteOnlyTransaction();
+        rwTx.merge(LogicalDatastoreType.OPERATIONAL, tpIid, tp);
+        assertCommit(rwTx.submit());
+
+        synchronized (waitObject) {
+             waitObject.wait(1500);
+        }
+
+        ForwardingAdjAnnounceInputBuilder forwardingAdjAnnounceInputBuilder = new ForwardingAdjAnnounceInputBuilder();
+        AnnouncementContextBuilder announcementContextBuilder = new AnnouncementContextBuilder();
+        announcementContextBuilder.setId(new Uri("test"));
+        HeadEndBuilder headEndBuilder = new HeadEndBuilder();
+        headEndBuilder.setNode(nodeId1);
+        headEndBuilder.setTpId(tpId1);
+        List<TpId> lTpId = new ArrayList<TpId>();
+        TpId supportingTpId = new TpId("supporting:1");
+        lTpId.add(supportingTpId);
+        headEndBuilder.setSupportingTp(lTpId);
+
+        TailEndBuilder tailEndBuilder = new TailEndBuilder();
+        tailEndBuilder.setNode(nodeId2);
+        tailEndBuilder.setTpId(tpId2);
+        lTpId = new ArrayList<TpId>();
+        supportingTpId = new TpId("supporting:2");
+        lTpId.add(supportingTpId);
+        tailEndBuilder.setSupportingTp(lTpId);
+
+        Long metric = 10L;
+        final MtLinkMetricAttributeValueBuilder mtLinkMetricAVBuilder = new MtLinkMetricAttributeValueBuilder();
+        mtLinkMetricAVBuilder.setMetric(metric);
+        final ValueBuilder valueBuilder = new ValueBuilder();
+        valueBuilder.addAugmentation(MtLinkMetricAttributeValue.class, mtLinkMetricAVBuilder.build());
+        final AttributeBuilder attributeBuilder = new AttributeBuilder();
+        attributeBuilder.setAttributeType(NativeL3IgpMetric.class);
+        attributeBuilder.setValue(valueBuilder.build());
+        final Uri uri = new Uri("test:1");
+        attributeBuilder.setId(uri);
+        final AttributeKey attributeKey = new AttributeKey(uri);
+        attributeBuilder.setKey(attributeKey);
+
+        List<Attribute> lAttribute = new ArrayList<Attribute>();
+        lAttribute.add(attributeBuilder.build());
+
+        NetworkTopologyRef networkTopologyRef = new NetworkTopologyRef(topologyIid);
+        forwardingAdjAnnounceInputBuilder.setAnnouncementContext(announcementContextBuilder.build());
+        forwardingAdjAnnounceInputBuilder.setAttribute(lAttribute);
+        UnidirectionalBuilder unidirectionalBuilder = new UnidirectionalBuilder();
+        org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.fa.parameters.directionality.info.UnidirectionalBuilder
+                unidirBuilder = new org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.fa.parameters.directionality.info.UnidirectionalBuilder();
+        unidirBuilder.setUnidirectional(unidirectionalBuilder.build());
+        forwardingAdjAnnounceInputBuilder.setDirectionalityInfo(unidirBuilder.build());
+        forwardingAdjAnnounceInputBuilder.setHeadEnd(headEndBuilder.build());
+        forwardingAdjAnnounceInputBuilder.setTailEnd(tailEndBuilder.build());
+        forwardingAdjAnnounceInputBuilder.setNetworkTopologyRef(networkTopologyRef);
+
+        Future<RpcResult<ForwardingAdjAnnounceOutput>> futureAnnounce =
+                provider.forwardingAdjAnnounce(forwardingAdjAnnounceInputBuilder.build());
+        RpcResult<ForwardingAdjAnnounceOutput> output = futureAnnounce.get();
+        assertNotNull(output);
+        ForwardingAdjAnnounceOutput resultAnnounceOutput = output.getResult();
+        org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.forwarding.adj.announce.output.Result
+                resultOutput = resultAnnounceOutput.getResult();
+        boolean b = resultOutput instanceof FaId;
+        assertTrue(b);
+
+        org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.FaId faId = ((FaId)resultOutput).getFaId();
+
+        ForwardingAdjWithdrawInputBuilder forwardingAdjWithdrawInputBuilder = new ForwardingAdjWithdrawInputBuilder();
+        forwardingAdjWithdrawInputBuilder.setFaId(faId);
+        forwardingAdjWithdrawInputBuilder.setNetworkTopologyRef(networkTopologyRef);
+
+        Future<RpcResult<ForwardingAdjWithdrawOutput>> futureWithdraw =
+               provider.forwardingAdjWithdraw(forwardingAdjWithdrawInputBuilder.build());
+        RpcResult<ForwardingAdjWithdrawOutput> withdraw = futureWithdraw.get();
+        assertNotNull(withdraw);
+        ForwardingAdjWithdrawOutput resultAdjWithdraw = withdraw.getResult();
+        org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.forwarding.adj.withdraw.output.Result
+                resultWithdraw = resultAdjWithdraw.getResult();
+        b = resultWithdraw instanceof org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multilayer.rev150123.forwarding.adj.withdraw.output.result.Ok;
+        assertTrue(b);
     }
 
     @After
