8bcd47dfdcc9f3184a242ba9e13d9c0469152336
Andrej Záň <andrej.zan@pantheon.sk>
2015-10-05 14:34:20 +0200

    Link computation input
    Change-Id: I1748a49d06c0648939568992cf7a562299265d24
    Signed-off-by: Andrej Záň <andrej.zan@pantheon.sk>

--- topoprocessing-inventory/src/main/java/org/opendaylight/topoprocessing/inventory/request/InvTopologyRequestHandler.java
+++ topoprocessing-inventory/src/main/java/org/opendaylight/topoprocessing/inventory/request/InvTopologyRequestHandler.java
@@ -15,11 +15,18 @@ import org.opendaylight.topoprocessing.impl.request.TopologyRequestHandler;
 import org.opendaylight.topoprocessing.impl.rpc.RpcServices;
 import org.opendaylight.topoprocessing.impl.util.GlobalSchemaContextHolder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.CorrelationAugment;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.CorrelationItemEnum;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.Model;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.correlations.grouping.Correlations;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.link.computation.rev150824.LinkComputationAugment;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.link.computation.rev150824.link.computation.grouping.LinkComputation;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Link;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.InstanceIdentifierBuilder;
 
 /**
  * @author matej.perina
@@ -47,6 +54,34 @@ public class InvTopologyRequestHandler extends TopologyRequestHandler{
     protected Class<? extends Model> getModel(Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode) {
         return ((Topology) fromNormalizedNode.getValue()).getAugmentation(CorrelationAugment.class)
                 .getCorrelations().getOutputModel();
+    }
+
+    @Override
+    protected LinkComputation getLinkComputation(Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode) {
+        Topology topo = ((Topology) fromNormalizedNode.getValue());
+        LinkComputationAugment linkCompAug = topo.getAugmentation(LinkComputationAugment.class);
+        if (linkCompAug == null) {
+            return null;
+        }
+        return linkCompAug.getLinkComputation();
+    }
+
+    @Override
+    protected YangInstanceIdentifier buildListenerIdentifier(InstanceIdentifierBuilder builder,
+            CorrelationItemEnum correlationItemEnum) {
+        switch (correlationItemEnum) {
+        case Node:
+        case TerminationPoint:
+            builder.node(Node.QNAME);
+            break;
+        case Link:
+            builder.node(Link.QNAME);
+            break;
+        default:
+            throw new IllegalArgumentException("Wrong Correlation item set: "
+                    + correlationItemEnum);
+        }
+        return builder.build();
     }
 
 }
