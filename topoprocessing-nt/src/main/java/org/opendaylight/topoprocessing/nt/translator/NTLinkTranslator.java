92fc9c4bcce9caaf221346c50e66974c1ad3c61c
Matej Perina <matej.perina@pantheon.sk>
2015-10-08 11:37:28 +0200

    Link computation output part
    Change-Id: I7cd91f362fc9a9c3b4094948b74897211341b9f5
    Signed-off-by: Matej Perina <matej.perina@pantheon.sk>

--- topoprocessing-nt/src/main/java/org/opendaylight/topoprocessing/nt/translator/NTLinkTranslator.java
+++ topoprocessing-nt/src/main/java/org/opendaylight/topoprocessing/nt/translator/NTLinkTranslator.java
@@ -10,18 +10,25 @@ package org.opendaylight.topoprocessing.nt.translator;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.opendaylight.topoprocessing.api.structure.ComputedLink;
 import org.opendaylight.topoprocessing.api.structure.OverlayItem;
 import org.opendaylight.topoprocessing.api.structure.UnderlayItem;
 import org.opendaylight.topoprocessing.impl.structure.OverlayItemWrapper;
 import org.opendaylight.topoprocessing.impl.translator.LinkTranslator;
 import org.opendaylight.topoprocessing.impl.util.TopologyQNames;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.link.attributes.Destination;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.link.attributes.Source;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.link.attributes.SupportingLink;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Link;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
 import org.opendaylight.yangtools.yang.data.api.schema.MapNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.CollectionNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableContainerNodeBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,7 +48,6 @@ public class NTLinkTranslator implements LinkTranslator{
                 SupportingLink.QNAME);
         // iterate through overlay items containing lists
         for (OverlayItem overlayItem : wrapper.getOverlayItems()) {
-            // TODO - add source and destination translation
             // iterate through underlay items
             for (UnderlayItem underlayItem : overlayItem.getUnderlayItems()) {
                 if (! writtenLinks.contains(underlayItem)) {
@@ -51,6 +57,28 @@ public class NTLinkTranslator implements LinkTranslator{
                             TopologyQNames.LINK_REF, underlayItem.getItemId()).build());
                 }
             }
+        }
+
+        UnderlayItem link = wrapper.getOverlayItems().get(0).getUnderlayItems().get(0);
+        if(link instanceof ComputedLink) {
+            ComputedLink computedLink = (ComputedLink) link;
+            ContainerNode sourceNode = ImmutableContainerNodeBuilder.create()
+                    .withNodeIdentifier(YangInstanceIdentifier.NodeIdentifier.create(Source.QNAME))
+                    .withChild(ImmutableNodes.leafNode(TopologyQNames.LINK_SOURCE_NODE_QNAME,
+                            NormalizedNodes.findNode(computedLink.getSrcNode(),
+                                    YangInstanceIdentifier.of(TopologyQNames.NETWORK_NODE_ID_QNAME)).get().getValue()))
+                    .build();
+            ContainerNode destNode = ImmutableContainerNodeBuilder.create()
+                    .withNodeIdentifier(YangInstanceIdentifier.NodeIdentifier.create(Destination.QNAME))
+                    .withChild(ImmutableNodes.leafNode(TopologyQNames.LINK_DEST_NODE_QNAME,
+                            NormalizedNodes.findNode(computedLink.getDstNode(),
+                                    YangInstanceIdentifier.of(TopologyQNames.NETWORK_NODE_ID_QNAME)).get().getValue()))
+                    .build();
+            return ImmutableNodes.mapEntryBuilder(Link.QNAME, TopologyQNames.NETWORK_LINK_ID_QNAME, wrapper.getId())
+                    .withChild(sourceNode)
+                    .withChild(destNode)
+                    .withChild(supportingLinks.build())
+                    .build();
         }
 
         return ImmutableNodes.mapEntryBuilder(Link.QNAME, TopologyQNames.NETWORK_LINK_ID_QNAME, wrapper.getId())
