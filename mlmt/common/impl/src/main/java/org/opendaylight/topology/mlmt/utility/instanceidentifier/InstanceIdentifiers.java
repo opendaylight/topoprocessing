b81a34260c9235260f11a894a64d89fba1301b7d
giorgiogarziano <giorgio.garziano@ericsson.com>
2016-02-26 17:24:30 +0100

    Mlmt: added instance identifier utilities
    Change-Id: Ifb93a22c326c7827c9cbca35fb80c7d4d62d4909
    Signed-off-by: giorgiogarziano <giorgio.garziano@ericsson.com>

---
+++ mlmt/common/impl/src/main/java/org/opendaylight/topology/mlmt/utility/instanceidentifier/InstanceIdentifiers.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2015 Ericsson, AB.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.topology.mlmt.utility.instanceidentifier;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.PathArgument;
+
+import com.google.common.base.Predicate;
+
+public class InstanceIdentifiers {
+
+    @SuppressWarnings("unchecked")
+    public static <T extends DataObject> InstanceIdentifier<T> copy(InstanceIdentifier<T> iid) {
+        List<PathArgument> pathArgs = new LinkedList<>();
+        for (PathArgument parg : iid.getPathArguments()) {
+            pathArgs.add(parg);
+        }
+
+        return (InstanceIdentifier<T>) InstanceIdentifier.<T>create(pathArgs);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends DataObject> InstanceIdentifier<T> transform(
+            InstanceIdentifier<T> iid, Transformer<InstanceIdentifier.PathArgument,
+                    InstanceIdentifier.PathArgument> transformer) {
+        if ( transformer == null ) {
+            return null;
+        }
+        List<PathArgument> pathArgs = new LinkedList<>();
+
+        for (PathArgument parg : iid.getPathArguments()) {
+            PathArgument newParg = transformer.transform(parg);
+            if (newParg != null) {
+                pathArgs.add(newParg);
+            }
+        }
+
+        return (InstanceIdentifier<T>) InstanceIdentifier.<T>create(pathArgs);
+    }
+
+    public static <T extends DataObject> Collection<InstanceIdentifier.PathArgument>
+            find(InstanceIdentifier<T> iid, Predicate<InstanceIdentifier.PathArgument> predicate) {
+        if (predicate == null) {
+            return null;
+        }
+        List<PathArgument> pathArgs = new LinkedList<>();
+
+        for (PathArgument parg : iid.getPathArguments()) {
+            if (predicate.apply(parg)) {
+                pathArgs.add(parg);
+            }
+        }
+
+        return pathArgs;
+    }
+
+    public static <T extends DataObject> InstanceIdentifier.PathArgument
+            findFirst(InstanceIdentifier<T> iid, Predicate<InstanceIdentifier.PathArgument> predicate) {
+        if (predicate == null) {
+            return null;
+        }
+
+        for (PathArgument parg : iid.getPathArguments()) {
+            if (predicate.apply(parg)) {
+                return parg;
+            }
+        }
+
+        return null;
+    }
+
+    public static <T extends DataObject> InstanceIdentifier.PathArgument
+            findLast(InstanceIdentifier<T> iid, Predicate<InstanceIdentifier.PathArgument> predicate) {
+        if (predicate == null) {
+            return null;
+        }
+
+        PathArgument result = null;
+        for (PathArgument parg : iid.getPathArguments()) {
+            if (predicate.apply(parg)) {
+                result = parg;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ mlmt/common/impl/src/main/java/org/opendaylight/topology/mlmt/utility/instanceidentifier/ItemWithKey.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2015 Ericsson, AB.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.topology.mlmt.utility.instanceidentifier;
+
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.IdentifiableItem;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.PathArgument;
+
+import com.google.common.base.Predicate;
+
+public class ItemWithKey<T,K> implements Predicate<InstanceIdentifier.PathArgument> {
+
+    private Class<T> cls;
+    private K key;
+
+    public Class<T> getCls() {
+        return cls;
+    }
+
+    public void setCls(Class<T> cls) {
+        this.cls = cls;
+    }
+
+    public ItemWithKey<T, K> cls(Class<T> cls) {
+        this.cls = cls;
+        return this;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public ItemWithKey<T, K> key(K key) {
+        this.key = key;
+        return this;
+    }
+
+    @Override
+    public boolean apply(PathArgument input) {
+        if (cls != null) {
+            if (!cls.equals(input.getType())) {
+                return false;
+            }
+        }
+        if (key != null) {
+            if (input instanceof InstanceIdentifier.IdentifiableItem<?, ?>) {
+                InstanceIdentifier.IdentifiableItem<?, ?> idItem = (IdentifiableItem<?, ?>) input;
+                if (key.equals(idItem.getKey())) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
