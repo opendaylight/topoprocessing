66b14b0b6b560d7a3c3bb16e566346efd4ad5e37
giorgiogarziano <giorgio.garziano@ericsson.com>
2016-03-15 16:31:39 +0100

    Mlmt: added mlmt node name handler
    Change-Id: Ibc2a8ee735d7358616be9d84a289ee83bdd9830c
    Signed-off-by: giorgiogarziano <giorgio.garziano@ericsson.com>

---
+++ mlmt/multitechnology-topology/impl/src/test/java/org/opendaylight/topology/multitechnology/MultitechnologyNodeNameHandlerTest.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) 2015 Ericsson, AB.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.topology.multitechnology;
+
+import com.google.common.base.Optional;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+
+import org.junit.runner.RunWith;
+import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.binding.test.AbstractDataBrokerTest;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopologyBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.TopologyTypes;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.TopologyTypesBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.UnderlayTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.UnderlayTopologyBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.UnderlayTopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
+import org.opendaylight.topology.mlmt.utility.MlmtOperationProcessor;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MultitechnologyNodeNameHandlerTest extends AbstractDataBrokerTest {
+
+    private final Object waitObject = new Object();
+    private static final String MLMT = "mlmt:1";
+    private static final String EXAMPLE = "example:1";
+    private DataBroker dataBroker;
+    private Thread thread;
+    private MlmtOperationProcessor processor;
+    private Topology mlmtTopology;
+    InstanceIdentifier<Topology> mlmtTopologyIid;
+    TopologyKey mlmtTopologyKey;
+
+    public class ChangeListener implements DataChangeListener {
+
+        @Override
+        public void onDataChanged(final AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change) {
+            synchronized (waitObject) {
+                waitObject.notify();
+            }
+        }
+    }
+
+    private InstanceIdentifier<Topology> buildTopologyIid(final String topologyName) {
+        TopologyId tid = new TopologyId(topologyName);
+        TopologyKey key = new TopologyKey(Preconditions.checkNotNull(tid));
+        return InstanceIdentifier.create(NetworkTopology.class).child(Topology.class, key);
+    }
+
+    private Topology buildMlmtTopology(final String topologyName) {
+        TopologyId topologyId = new TopologyId(topologyName);
+        mlmtTopologyKey = new TopologyKey(Preconditions.checkNotNull(topologyId));
+        final UnderlayTopologyBuilder underlayTopologyBuilder = new UnderlayTopologyBuilder();
+        final TopologyTypesBuilder topologyTypesBuilder = new TopologyTypesBuilder();
+        TopologyId underlayTopologyRef = new TopologyId(EXAMPLE);
+        underlayTopologyBuilder.setTopologyRef(underlayTopologyRef);
+        UnderlayTopologyKey underlayKey = new UnderlayTopologyKey(underlayTopologyRef);
+        underlayTopologyBuilder.setKey(underlayKey);
+        UnderlayTopology underlayTopology = underlayTopologyBuilder.build();
+        List<UnderlayTopology> lUnderlayTopology = new ArrayList<UnderlayTopology>();
+        lUnderlayTopology.add(underlayTopology);
+        final TopologyBuilder tbuilder = new TopologyBuilder();
+        tbuilder.setKey(mlmtTopologyKey);
+        tbuilder.setTopologyId(topologyId);
+        final Topology top = tbuilder.setServerProvided(Boolean.FALSE)
+                .setTopologyTypes(topologyTypesBuilder.build())
+                .setUnderlayTopology(lUnderlayTopology).build();
+
+        return top;
+    }
+
+    @Override
+    protected void setupWithDataBroker(DataBroker dataBroker) {
+        this.dataBroker = dataBroker;
+    }
+
+    @BeforeClass
+    public static void allMethodsSetUp() {
+        // NOOP
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        processor = new MlmtOperationProcessor(dataBroker);
+        thread = new Thread(processor);
+        thread.setDaemon(true);
+        thread.setName("MlmtOperationProcessor");
+        thread.start();
+
+        mlmtTopologyIid = buildTopologyIid(MLMT);
+        /*
+         * It is necessary to create the network-topology containers in
+         * both configuration and operational data storage
+         */
+        NetworkTopologyBuilder nb = new NetworkTopologyBuilder();
+        NetworkTopology networkTopology = nb.build();
+        WriteTransaction rwTx = dataBroker.newWriteOnlyTransaction();
+        rwTx.put(LogicalDatastoreType.OPERATIONAL,
+                InstanceIdentifier.create(NetworkTopology.class), networkTopology);
+        assertCommit(rwTx.submit());
+
+        rwTx = dataBroker.newWriteOnlyTransaction();
+        rwTx.put(LogicalDatastoreType.CONFIGURATION,
+                InstanceIdentifier.create(NetworkTopology.class), networkTopology);
+        assertCommit(rwTx.submit());
+
+        dataBroker.registerDataChangeListener(LogicalDatastoreType.OPERATIONAL,
+                mlmtTopologyIid, new ChangeListener(), DataBroker.DataChangeScope.SUBTREE);
+
+        mlmtTopology = buildMlmtTopology(MLMT);
+        rwTx = dataBroker.newWriteOnlyTransaction();
+        rwTx.merge(LogicalDatastoreType.OPERATIONAL, mlmtTopologyIid, mlmtTopology, true);
+        assertCommit(rwTx.submit());
+
+        ReadOnlyTransaction rTx = dataBroker.newReadOnlyTransaction();
+        Optional<Topology> optional = rTx.read(LogicalDatastoreType.OPERATIONAL, mlmtTopologyIid).get();
+        assertNotNull(optional);
+        assertTrue("Operational mlmt:1 topology ", optional.isPresent());
+        Topology rxTopology = optional.get();
+        assertNotNull(rxTopology);
+
+        NodeBuilder nodeBuilder = new NodeBuilder();
+        String nodeName = "node:1";
+        NodeId nodeId = new NodeId(nodeName);
+        nodeBuilder.setNodeId(nodeId);
+        NodeKey nodeKey = new NodeKey(nodeId);
+        nodeBuilder.setKey(nodeKey);
+        InstanceIdentifier<Node> nodeIid = mlmtTopologyIid.child(Node.class, nodeKey);
+
+        rwTx = dataBroker.newWriteOnlyTransaction();
+        rwTx.put(LogicalDatastoreType.OPERATIONAL, nodeIid, nodeBuilder.build());
+        assertCommit(rwTx.submit());
+}
+
+    @Test
+    public void onNodeNameTest() {
+        String nodeName = "node:1";
+        NodeId nodeId = new NodeId(nodeName);
+        NodeKey nodeKey = new NodeKey(nodeId);
+
+        MultitechnologyNodeNameHandler.putNodeName(dataBroker, processor,
+                mlmtTopologyIid, nodeKey, nodeName, "NODE-NAME");
+
+        nodeName = "node:2";
+        MultitechnologyNodeNameHandler.putNodeName(dataBroker, processor,
+                mlmtTopologyIid, nodeKey, nodeName, "NODE-NAME");
+    }
+
+    @After
+    public void clear() {
+        // NOOP
+    }
+
+    @AfterClass
+    public static void allMethodsClear() {
+        // NOOP
+    }
+}
