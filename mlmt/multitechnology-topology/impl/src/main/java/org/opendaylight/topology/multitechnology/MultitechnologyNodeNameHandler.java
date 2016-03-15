1b96195b9b4c69a0af1796f1ef29485a5e99cf6f
giorgiogarziano <giorgio.garziano@ericsson.com>
2016-03-15 16:31:39 +0100

    Mlmt: added mlmt node name handler
    Change-Id: Ibc2a8ee735d7358616be9d84a289ee83bdd9830c
    Signed-off-by: giorgiogarziano <giorgio.garziano@ericsson.com>

---
+++ mlmt/multitechnology-topology/impl/src/main/java/org/opendaylight/topology/multitechnology/MultitechnologyNodeNameHandler.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2015 Ericsson, AB.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.topology.multitechnology;
+
+import com.google.common.base.Optional;
+
+import java.util.concurrent.ExecutionException;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.rev150122.MtInfoNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.rev150122.MtInfoNodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.rev150122.mt.info.AttributeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.rev150122.mt.info.Attribute;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.rev150122.mt.info.AttributeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.rev150122.mt.info.attribute.Value;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.rev150122.mt.info.attribute.ValueBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.opaque.attribute.rev150122.opaque.attribute.value.basic.attribute.types.StringValueBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.opaque.attribute.rev150122.MtOpaqueNodeAttributeValueBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.opaque.attribute.rev150122.MtOpaqueNodeAttributeValue;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.multitechnology.opaque.attribute.rev150122.Controller;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Uri;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.opendaylight.topology.mlmt.utility.MlmtOperationProcessor;
+import org.opendaylight.topology.mlmt.utility.MlmtTopologyOperation;
+
+public class MultitechnologyNodeNameHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MultitechnologyNodeNameHandler.class);
+
+    public static void putNodeName(final DataBroker dataProvider, final MlmtOperationProcessor processor,
+            final InstanceIdentifier<Topology> topologyInstanceId, final NodeKey nodeKey,
+                    final String nodeName, final String nodeNameField) {
+        StringValueBuilder stringValueBuilder = new StringValueBuilder();
+        stringValueBuilder.setStringValue(nodeName);
+        MtOpaqueNodeAttributeValueBuilder mtOpaqueNodeAttributeValueBuilder =
+                new MtOpaqueNodeAttributeValueBuilder();
+        mtOpaqueNodeAttributeValueBuilder.setBasicAttributeTypes(stringValueBuilder.build());
+
+        final String path = nodeNameField;
+        final Uri uri = new Uri(path);
+        final AttributeKey attributeKey = new AttributeKey(uri);
+        final InstanceIdentifier<Attribute> instanceAttributeId = topologyInstanceId.child(Node.class, nodeKey)
+                .augmentation(MtInfoNode.class).child(Attribute.class, attributeKey);
+        final ValueBuilder valueBuilder = new ValueBuilder();
+        valueBuilder.addAugmentation(MtOpaqueNodeAttributeValue.class, 
+                mtOpaqueNodeAttributeValueBuilder.build());
+        final AttributeBuilder attributeBuilder = new AttributeBuilder();
+        attributeBuilder.setAttributeType(Controller.class);
+        attributeBuilder.setValue(valueBuilder.build());
+        attributeBuilder.setKey(attributeKey);
+
+        final InstanceIdentifier<Topology> targetTopologyId = topologyInstanceId;
+
+        try {
+            final ReadOnlyTransaction rx = dataProvider.newReadOnlyTransaction();
+            final Optional<Attribute> sourceAttributeObject =
+                rx.read(LogicalDatastoreType.OPERATIONAL, instanceAttributeId).get();
+
+            processor.enqueueOperation(new MlmtTopologyOperation() {
+                @Override
+                public void applyOperation(ReadWriteTransaction transaction) {
+                    if (sourceAttributeObject != null && sourceAttributeObject.isPresent()
+                            && sourceAttributeObject.get() != null) {
+                        final Value value = sourceAttributeObject.get().getValue();
+                        final MtOpaqueNodeAttributeValue mtOpaqueNodeAttributeValue =
+                                value.getAugmentation(MtOpaqueNodeAttributeValue.class);
+                        transaction.put(LogicalDatastoreType.OPERATIONAL,
+                                instanceAttributeId, attributeBuilder.build());
+                    } else {
+                        final MtInfoNodeBuilder mtInfoNodeBuilder = new MtInfoNodeBuilder();
+                        final List<Attribute> la = new ArrayList<Attribute>();
+                        la.add(attributeBuilder.build());
+                        mtInfoNodeBuilder.setAttribute(la);
+                        final InstanceIdentifier<MtInfoNode> instanceId =
+                                topologyInstanceId.child(Node.class, nodeKey).augmentation(MtInfoNode.class);
+                        transaction.merge(LogicalDatastoreType.OPERATIONAL, instanceId,
+                                mtInfoNodeBuilder.build(), true);
+                    }
+                }
+            });
+
+        } catch (final InterruptedException e) {
+            LOG.error("MultitechnologyNodeNameHandler.putNodeName interrupted exception", e);
+        } catch (final ExecutionException e) {
+            LOG.error("MultitechnologyNodeNameHandler.putNodeName execution exception", e);
+        }
+    }
+}
+
