c065a983cfb0ede0beea568612e82f34d9ff3ec3
tomas.vahancik <tomas.vahancik@pantheon.tech>
2016-03-21 17:53:29 +0100

    Fix for bug 4674
    Also fix for tp filtration where tp-ref was incorrect.
    
    Change-Id: I62947931363913534cee3df77c73db37479b57f1
    Signed-off-by: tomas.vahancik <tomas.vahancik@pantheon.tech>
    Signed-off-by: Samuel Kontriš <samuel.kontris@pantheon.tech>

--- topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/util/TopologyQNames.java
+++ topoprocessing-impl/src/main/java/org/opendaylight/topoprocessing/impl/util/TopologyQNames.java
@@ -103,7 +103,7 @@ public final class TopologyQNames {
     public static final QName INVENTORY_NODE_CONNECTOR_REF_QNAME =
             QName.create("urn:opendaylight:model:topology:inventory", "2013-10-30", "inventory-node-connector-ref");
     public static final QName INVENTORY_NODE_REF_QNAME = QName
-            .create("(urn:opendaylight:model:topology:inventory?revision=2013-10-30)inventory-node-ref");
+            .create("urn:opendaylight:model:topology:inventory", "2013-10-30", "inventory-node-ref");
 
     /**Correlation augment QName**/
     public static final QName TOPOLOGY_CORRELATION_AUGMENT =
