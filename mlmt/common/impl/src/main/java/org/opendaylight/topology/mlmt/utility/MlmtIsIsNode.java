196d3ae059a808db650a1ee40bd6556e85e89a00
giorgiogarziano <giorgio.garziano@ericsson.com>
2016-03-23 09:09:07 +0100

    Mlmt: added common utility classes
    Change-Id: I1c4ec92686cdb25ff843e00f260eea759efd6c0e
    Signed-off-by: giorgiogarziano <giorgio.garziano@ericsson.com>

---
+++ mlmt/common/impl/src/main/java/org/opendaylight/topology/mlmt/utility/MlmtIsIsNode.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2015 Ericsson, AB.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.topology.mlmt.utility;
+
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.ted.rev131021.TedNodeAttributes;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.nt.l3.unicast.igp.topology.rev131021.Node1;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.isis.topology.rev131021.IgpNodeAttributes1;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.nt.l3.unicast.igp.topology.rev131021.igp.node.attributes.IgpNodeAttributes;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.isis.topology.rev131021.isis.node.attributes.IsisNodeAttributes;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.isis.topology.rev131021.isis.node.attributes.isis.node.attributes.Iso;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.isis.topology.rev131021.IsoSystemId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.isis.topology.rev131021.isis.node.attributes.isis.node.attributes.Ted;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+
+public class MlmtIsIsNode {
+
+    static IsoSystemId isoSystemId(final Node node) {
+        final Node1 node1 = node.getAugmentation(Node1.class);
+        if (node1 == null) {
+            return null;
+        }
+        final IgpNodeAttributes igpNodeAttributes = node1.getIgpNodeAttributes();
+        if (igpNodeAttributes == null) {
+            return null;
+        }
+        IgpNodeAttributes1 igpNodeAttributes1 = igpNodeAttributes.getAugmentation(IgpNodeAttributes1.class);
+        if (igpNodeAttributes1 == null) {
+            return null;
+        }
+        IsisNodeAttributes isisNodeAttributes = igpNodeAttributes1.getIsisNodeAttributes();
+        if (isisNodeAttributes == null) {
+            return null;
+        }
+        Iso iso = isisNodeAttributes.getIso();
+        if (iso == null) {
+            return null;
+        }
+
+        return iso.getIsoSystemId();
+    }
+
+    static Ipv4Address ipv4Address(final Node node) {
+         final Node1 node1 = node.getAugmentation(Node1.class);
+         if (node1 == null) {
+             return null;
+         }
+         final IgpNodeAttributes igpNodeAttributes = node1.getIgpNodeAttributes();
+         if (igpNodeAttributes == null) {
+            return null;
+         }
+         IgpNodeAttributes1 igpNodeAttributes1 = igpNodeAttributes.getAugmentation(IgpNodeAttributes1.class);
+         if (igpNodeAttributes1 == null) {
+             return null;
+         }
+         IsisNodeAttributes isisNodeAttributes = igpNodeAttributes1.getIsisNodeAttributes();
+         if (isisNodeAttributes == null) {
+             return null;
+         }
+         TedNodeAttributes tedNodeAttributes = isisNodeAttributes.getTed();
+         if (tedNodeAttributes == null) {
+            return null;
+         }
+
+         return tedNodeAttributes.getTeRouterIdIpv4();
+    }
+}
---
+++ mlmt/common/impl/src/main/java/org/opendaylight/topology/mlmt/utility/MlmtLinkNameObserver.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2015 Ericsson, AB.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.topology.mlmt.utility;
+
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.LinkKey;
+
+public interface MlmtLinkNameObserver {
+
+    void notifyLinkName(final InstanceIdentifier<Topology> topologyId, final LinkKey linkKey,
+            final String linkName);
+}
