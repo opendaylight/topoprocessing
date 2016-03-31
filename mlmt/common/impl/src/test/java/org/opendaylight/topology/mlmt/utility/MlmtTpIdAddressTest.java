7ac0c530eecc155b189d2082f5c6abb306175843
giorgiogarziano <giorgio.garziano@ericsson.com>
2016-03-31 09:52:16 +0200

    Mlmt: ipv4 address utility update
    Change-Id: I3b83c11825321d04ece7dc074afb0b2fd25ec935
    Signed-off-by: giorgiogarziano <giorgio.garziano@ericsson.com>

--- mlmt/common/impl/src/test/java/org/opendaylight/topology/mlmt/utility/MlmtTpIdAddressTest.java
+++ mlmt/common/impl/src/test/java/org/opendaylight/topology/mlmt/utility/MlmtTpIdAddressTest.java
@@ -14,13 +14,14 @@ import org.junit.Test;
 import org.junit.After;
 import org.junit.AfterClass;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPointBuilder;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TpId;
 
-public class MlmtTpIdAddressTest{
+public class MlmtTpIdAddressTest {
 
     @BeforeClass
     public static void allMethodsSetUp() {
@@ -34,20 +35,74 @@ public class MlmtTpIdAddressTest{
 
     @Test
     public void testTpIpAddress() throws Exception {
-
-        final String fakeBgp = "bgpls://IsisLevel1:0/type=tp&ipv4=10.0.1.11&id=0";
-        final String realBgp = "bgpls://IsisLevel1:100/type=tp&mt=0&ipv4=172.16.15.1";
-
+        // valid ipv4-address
+        String fakeBgp = "bgpls://IsisLevel1:0/type=tp&ipv4=172.16.15.1&id=0";
         final TerminationPointBuilder tpBuilder = new TerminationPointBuilder();
         tpBuilder.setTpId(new TpId(fakeBgp));
         String ipAddress = MlmtTpIdIpAddress.ipv4Address(tpBuilder.build());
         assertNotNull(ipAddress);
-        assertEquals(ipAddress, "10.0.1.11");
+        assertEquals(ipAddress, "172.16.15.1");
 
+        // valid ipv4-address
+        fakeBgp = "bgpls://IsisLevel1:0/type=tp&ipv4=10.0.0.8&id=0";
+        tpBuilder.setTpId(new TpId(fakeBgp));
+        ipAddress = MlmtTpIdIpAddress.ipv4Address(tpBuilder.build());
+        assertNotNull(ipAddress);
+        assertEquals(ipAddress, "10.0.0.8");
+
+        // invalid ipv4-address
+        fakeBgp = "bgpls://IsisLevel1:100/type=tp&mt=0&ipv4=0.0.0.8&id=0";
+        tpBuilder.setTpId(new TpId(fakeBgp));
+        ipAddress = MlmtTpIdIpAddress.ipv4Address(tpBuilder.build());
+        assertNotNull(ipAddress);
+        assertEquals(ipAddress, "0.0.0.8");
+
+        // valid ipv4-address (see RFC 5735)
+        fakeBgp = "bgpls://IsisLevel1:100/type=tp&mt=0&ipv4=300.0.0.8&id=0";
+        tpBuilder.setTpId(new TpId(fakeBgp));
+        ipAddress = MlmtTpIdIpAddress.ipv4Address(tpBuilder.build());
+        assertNull(ipAddress);
+
+        // null and valid ipv4-address
+        fakeBgp = "bgpls://IsisLevel1:100/type=tp&mt=0&ipv4=0.0.0.0";
+        tpBuilder.setTpId(new TpId(fakeBgp));
+        ipAddress = MlmtTpIdIpAddress.ipv4Address(tpBuilder.build());
+        assertNotNull(ipAddress);
+        assertEquals(ipAddress, "0.0.0.0");
+
+        // valid ipv4-address
+        String realBgp = "bgpls://IsisLevel1:100/type=tp&mt=0&ipv4=172.16.15.1";
         tpBuilder.setTpId(new TpId(realBgp));
         ipAddress = MlmtTpIdIpAddress.ipv4Address(tpBuilder.build());
         assertNotNull(ipAddress);
         assertEquals(ipAddress, "172.16.15.1");
+
+        // valid ipv4-address
+        realBgp = "bgpls://IsisLevel1:100/type=tp&mt=0&ipv4=10.0.0.8";
+        tpBuilder.setTpId(new TpId(realBgp));
+        ipAddress = MlmtTpIdIpAddress.ipv4Address(tpBuilder.build());
+        assertNotNull(ipAddress);
+        assertEquals(ipAddress, "10.0.0.8");
+
+        // valid ipv4-address (see RFC 5735)
+        realBgp = "bgpls://IsisLevel1:100/type=tp&mt=0&ipv4=0.0.0.8";
+        tpBuilder.setTpId(new TpId(realBgp));
+        ipAddress = MlmtTpIdIpAddress.ipv4Address(tpBuilder.build());
+        assertNotNull(ipAddress);
+        assertEquals(ipAddress, "0.0.0.8");
+
+        // invalid ipv4-address
+        realBgp = "bgpls://IsisLevel1:100/type=tp&mt=0&ipv4=300.0.0.8";
+        tpBuilder.setTpId(new TpId(realBgp));
+        ipAddress = MlmtTpIdIpAddress.ipv4Address(tpBuilder.build());
+        assertNull(ipAddress);
+
+        // null and valid ipv4-address
+        realBgp = "bgpls://IsisLevel1:100/type=tp&mt=0&ipv4=0.0.0.0";
+        tpBuilder.setTpId(new TpId(realBgp));
+        ipAddress = MlmtTpIdIpAddress.ipv4Address(tpBuilder.build());
+        assertNotNull(ipAddress);
+        assertEquals(ipAddress, "0.0.0.0");
     }
 
     @After
