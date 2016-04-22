bc5e2f205f52f90e6b433700d0fab6c60159163f
marek.korenciak <marek.korenciak@pantheon.tech>
2016-04-22 15:58:30 +0200

    Commit 2/2 of request coverage
    Change-Id: Iad5c3f72fffc8c2190feca7d3d8077265fb9e745
    Signed-off-by: marek.korenciak <marek.korenciak@pantheon.tech>

--- topoprocessing-impl/src/test/java/org/opendaylight/topoprocessing/impl/request/TopologyRequestHandlerTest.java
+++ topoprocessing-impl/src/test/java/org/opendaylight/topoprocessing/impl/request/TopologyRequestHandlerTest.java
@@ -1,6 +1,7 @@
 package org.opendaylight.topoprocessing.impl.request;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.mock;
@@ -19,11 +20,13 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataTreeChangeListener;
 import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
 import org.opendaylight.controller.md.sal.dom.broker.impl.PingPongDataBroker;
 import org.opendaylight.topoprocessing.api.filtration.FiltratorFactory;
 import org.opendaylight.topoprocessing.impl.adapter.ModelAdapter;
 import org.opendaylight.topoprocessing.impl.listener.UnderlayTopologyListener;
+import org.opendaylight.topoprocessing.impl.operator.LinkCalculator;
 import org.opendaylight.topoprocessing.impl.operator.TopologyAggregator;
 import org.opendaylight.topoprocessing.impl.operator.TopologyOperator;
 import org.opendaylight.topoprocessing.impl.operator.filtrator.AbstractFiltrator;
@@ -39,9 +42,11 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.FilterBase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.FiltrationAggregation;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.FiltrationOnly;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.I2rsModel;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.LeafPath;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.Model;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.NetworkTopologyModel;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.OpendaylightInventoryModel;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.RenderingOnly;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.correlations.grouping.Correlations;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.correlations.grouping.CorrelationsBuilder;
@@ -51,10 +56,17 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.correlations.grouping.correlations.correlation.FiltrationBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.correlations.grouping.correlations.correlation.Rendering;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.correlations.grouping.correlations.correlation.aggregation.Mapping;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.correlations.grouping.correlations.correlation.aggregation.MappingKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.correlations.grouping.correlations.correlation.aggregation.mapping.TargetField;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.correlations.grouping.correlations.correlation.aggregation.mapping.TargetFieldBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.correlations.grouping.correlations.correlation.filtration.Filter;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.link.computation.rev150824.link.computation.grouping.LinkComputation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.link.computation.rev150824.link.computation.grouping.link.computation.LinkInfo;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.link.computation.rev150824.link.computation.grouping.link.computation.LinkInfoKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.link.computation.rev150824.link.computation.grouping.link.computation.NodeInfo;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.Augmentation;
+import org.opendaylight.yangtools.yang.binding.DataContainer;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
@@ -97,6 +109,9 @@ public class TopologyRequestHandlerTest {
     private DOMRpcService domRpcServiceMock;
     @Mock
     private GlobalSchemaContextHolder schemaHolderMock;
+    @Mock
+    private TargetField targetFieldMock;
+
 
     private TestTopologyRequestHandler handler;
 
@@ -237,6 +252,134 @@ public class TopologyRequestHandlerTest {
         handler.setModelAdapters(modelAdaptersMock);
     }
 
+    @Test
+    public void testInitLinkComputation(){
+        LinkComputation linkComputationMock = mock(LinkComputation.class);
+        NodeInfo nodeInfoMock = mock(NodeInfo.class);
+        LeafPath leafPathMock = mock(LeafPath.class);
+        PathTranslator translartorMock = mock(PathTranslator.class);
+        InstanceIdentifierBuilder instanceIdentifierBuilderMock = mock(InstanceIdentifierBuilder.class);
+        ArrayList<LinkInfo> linksInformations = new ArrayList<LinkInfo>();
+        int iterator = 0;
+
+        initLinkComputationConfig(true);
+
+        setTestHandler();
+        handler.setLinkComputation(linkComputationMock);
+        handler.setTranslator(translartorMock);
+
+        when(modelAdaptersMock.get(I2rsModel.class)).thenReturn(modelAdapterMock);
+        when(modelAdapterMock.registerUnderlayTopologyListener((PingPongDataBroker)any(), (String)any(),
+                (CorrelationItemEnum)any(), (DatastoreType)any(),
+                (LinkCalculator)any(), (List<ListenerRegistration<DOMDataTreeChangeListener>>)any(),
+                (Map<Integer, YangInstanceIdentifier>)any())).thenReturn(mock(UnderlayTopologyListener.class));
+        when(modelAdapterMock.createTopologyIdentifier("underTopo1")).thenReturn(instanceIdentifierBuilderMock);
+        when(modelAdapterMock.createTopologyIdentifier("overTopo1")).thenReturn(instanceIdentifierBuilderMock);
+        when(modelAdapterMock.buildItemIdentifier(instanceIdentifierBuilderMock,
+                CorrelationItemEnum.Node)).thenReturn(mock(YangInstanceIdentifier.class));
+        when(modelAdapterMock.buildItemIdentifier(instanceIdentifierBuilderMock,
+                CorrelationItemEnum.Link)).thenReturn(mock(YangInstanceIdentifier.class));
+
+        when(linkComputationMock.getLinkInfo()).thenReturn(null);
+        when(linkComputationMock.getNodeInfo()).thenReturn(nodeInfoMock);
+        when(nodeInfoMock.getNodeTopology()).thenReturn("overTopo1");
+
+        when(targetFieldMock.getMatchingKey()).thenReturn(1);
+        when(targetFieldMock.getTargetFieldPath()).thenReturn(leafPathMock);
+        when(leafPathMock.getValue()).thenReturn("");
+
+        //linksInformations is null, exception should be thrown
+        try{
+            handler.processNewRequest();
+        }catch(IllegalStateException e){
+            iterator++;
+        }
+
+        when(linkComputationMock.getLinkInfo()).thenReturn(linksInformations);
+
+        //linksInformations is empty, exception should be thrown
+        try{
+            handler.processNewRequest();
+        }catch(IllegalStateException e){
+            iterator++;
+            verify(linkComputationMock, times(0)).getNodeInfo();
+        }
+
+        linksInformations.add(getLinkList());
+
+        //linksInformations is not empty, linkAggregation is null
+        handler.processNewRequest();
+        verify(translartorMock, times(0)).translate("", CorrelationItemEnum.Link, schemaHolderMock,
+                NetworkTopologyModel.class);
+        assertEquals(2, handler.getListeners().size());
+
+        initLinkComputationConfig(false);
+        handler.correctTopologyID = false;
+        handler.getListeners().clear();
+
+        //linksInformations is not empty, linkAggregation is not null, but
+        //mapping s underlayTopology is not equals
+        handler.processNewRequest();
+        verify(translartorMock, times(0)).translate("", CorrelationItemEnum.Link, schemaHolderMock,
+                NetworkTopologyModel.class);
+        assertEquals(2, handler.getListeners().size());
+
+        handler.correctTopologyID = true;
+        handler.getListeners().clear();
+
+        //linksInformations is not empty, linkAggregation is not null, mapping s
+        //underlayTopology is equals
+        handler.processNewRequest();
+        verify(translartorMock, times(1)).translate("", CorrelationItemEnum.Link, schemaHolderMock,
+                NetworkTopologyModel.class);
+        assertEquals(2, handler.getListeners().size());
+
+        verify(linkComputationMock, times(5)).getLinkInfo();
+        assertEquals(2, iterator);
+    }
+
+    private LinkInfo getLinkList(){
+        return new LinkInfo() {
+            @Override
+            public Class<? extends Model> getInputModel() {
+                return I2rsModel.class;
+            }
+
+            @Override
+            public <E extends Augmentation<LinkInfo>> E getAugmentation(Class<E> arg0) {
+                return null;
+            }
+
+            @Override
+            public Class<? extends DataContainer> getImplementedInterface() {
+                return null;
+            }
+
+            @Override
+            public Boolean isAggregatedLinks() {
+                return null;
+            }
+
+            @Override
+            public String getLinkTopology() {
+                return "underTopo1";
+            }
+
+            @Override
+            public LinkInfoKey getKey() {
+                return null;
+            }
+        };
+    }
+
+    private void initLinkComputationConfig(boolean agregationNull){
+        handler.aggregationOnly = true;
+        handler.agregationCorelationIntemLink = true;
+        handler.filtrationAggregation = false;
+        handler.realMapping = true;
+        handler.agregationNull = agregationNull;
+    }
+
     private class TestTopologyRequestHandler extends TopologyRequestHandler
     {
         private boolean dataMissing;
@@ -244,6 +387,11 @@ public class TopologyRequestHandlerTest {
         private boolean aggregationOnly;
         private boolean renderingOnly;
         private boolean filtrationAggregation;
+        private boolean agregationCorelationIntemLink;
+        private boolean agregationNull = false;
+        private boolean realMapping = false;
+        private boolean correctTopologyID = true;
+        private LinkComputation linkComputationReturn;
 
         public TestTopologyRequestHandler(DOMDataBroker dataBroker, GlobalSchemaContextHolder schemaHolder,
                 RpcServices rpcServices, Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode) {
@@ -266,7 +414,11 @@ public class TopologyRequestHandlerTest {
                List<Correlation> correlations = new ArrayList<Correlation>();
 
                CorrelationBuilder correlationBuilder = new CorrelationBuilder();
-               correlationBuilder.setCorrelationItem(CorrelationItemEnum.Node);
+               if(agregationCorelationIntemLink){
+                   correlationBuilder.setCorrelationItem(CorrelationItemEnum.Link);
+               }else{
+                   correlationBuilder.setCorrelationItem(CorrelationItemEnum.Node);
+               }
 
                if(filtrationOnly || filtrationAggregation){
                    FiltrationBuilder filtrationBuilder = new FiltrationBuilder();
@@ -284,9 +436,13 @@ public class TopologyRequestHandlerTest {
                    AggregationBuilder aggregationBuilder = new AggregationBuilder();
 
                    List<Mapping> mappings = new ArrayList<Mapping>();
-                   mappings.add(mappingMock);
-                   mappings.add(mappingMock);
-                   mappings.add(mappingMock);
+                   if(realMapping){
+                       mappings.add(getMapping(correctTopologyID));
+                   }else{
+                       mappings.add(mappingMock);
+                       mappings.add(mappingMock);
+                       mappings.add(mappingMock);
+                   }
                    aggregationBuilder.setMapping(mappings);
                    aggregationBuilder.setAggregationType(Equality.class);
 
@@ -294,7 +450,11 @@ public class TopologyRequestHandlerTest {
                        correlationBuilder.setType(FiltrationAggregation.class);
                    }
                    else correlationBuilder.setType(AggregationOnly.class);
-                   correlationBuilder.setAggregation(aggregationBuilder.build());
+                   if(agregationNull){
+                       correlationBuilder.setAggregation(null);
+                   }else{
+                       correlationBuilder.setAggregation(aggregationBuilder.build());
+                   }
 
                }
                if(renderingOnly){
@@ -314,7 +474,61 @@ public class TopologyRequestHandlerTest {
 
         @Override
         protected LinkComputation getLinkComputation(Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode) {
-            return null;
+            return linkComputationReturn;
+        }
+
+        public void setLinkComputation(LinkComputation linkComputation){
+            linkComputationReturn = linkComputation;
+        }
+
+        private Mapping getMapping(final boolean correctTopologyID){
+            return new Mapping() {
+                @Override
+                public Class<? extends Model> getInputModel() {
+                    return OpendaylightInventoryModel.class;
+                }
+
+                @Override
+                public <E extends Augmentation<Mapping>> E getAugmentation(Class<E> arg0) {
+                    return null;
+                }
+
+                @Override
+                public Class<? extends DataContainer> getImplementedInterface() {
+                    return null;
+                }
+
+                @Override
+                public Boolean isAggregateInside() {
+                    return null;
+                }
+
+                @Override
+                public String getUnderlayTopology() {
+                    if(correctTopologyID){
+                        return "underTopo1";
+                    }else{
+                        return "topology1";
+                    }
+                }
+
+                @Override
+                public List<TargetField> getTargetField() {
+                    ArrayList<TargetField> targetFieldList = new ArrayList<TargetField>();
+                    targetFieldList.add(targetFieldMock);
+                    return targetFieldList;
+                }
+
+                @Override
+                public MappingKey getKey() {
+                    return null;
+                }
+
+                @Override
+                public List<String> getApplyFilters() {
+                    return null;
+                }
+            };
         }
     }
 }
