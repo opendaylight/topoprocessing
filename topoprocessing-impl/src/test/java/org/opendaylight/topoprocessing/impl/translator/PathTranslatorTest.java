f736deb7246baf805004eb35143b3f7e143d4573
Tomáš Vahančík <tomas.vahancik@pantheon.tech>
2016-05-12 15:56:25 +0200

    PathTranslatorTest edit
    Change-Id: Ia5dcf7f63a8dd9305e73a1d770fb88272bf88dd9
    Signed-off-by: Tomáš Vahančík <tomas.vahancik@pantheon.tech>

--- topoprocessing-impl/src/test/java/org/opendaylight/topoprocessing/impl/translator/PathTranslatorTest.java
+++ topoprocessing-impl/src/test/java/org/opendaylight/topoprocessing/impl/translator/PathTranslatorTest.java
@@ -27,11 +27,14 @@ import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.opendaylight.topoprocessing.impl.util.GlobalSchemaContextHolder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.CorrelationItemEnum;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.I2rsModel;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.Model;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.NetworkTopologyModel;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.OpendaylightInventoryModel;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.AugmentationIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
 import org.opendaylight.yangtools.yang.data.util.DataSchemaContextTree;
 import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 import org.opendaylight.yangtools.yang.model.parser.api.YangSyntaxErrorException;
@@ -51,16 +54,36 @@ public class PathTranslatorTest {
     private PathTranslator pathTranslator = new PathTranslator();
     private SchemaContext schemaContext;
 
-    @Mock private GlobalSchemaContextHolder mockSchemaHolder;
+    @Mock
+    private GlobalSchemaContextHolder mockSchemaHolder;
 
     private Class<? extends Model> NTmodel = NetworkTopologyModel.class;
+    private Class<? extends Model> I2RSmodel = I2rsModel.class;
+    private Class<? extends Model> INVmodel = OpendaylightInventoryModel.class;
 
     private static SchemaContext createTestContext() throws IOException, YangSyntaxErrorException {
         final YangParserImpl parser = new YangParserImpl();
         List<String> modules = new ArrayList<String>();
         modules.add("/ietf-inet-types.yang");
         modules.add("/network-topology.yang");
+        modules.add("/l3-unicast-igp-topology.yang");
         modules.add("/network-topology-pcep.yang");
+        modules.add("/ietf-network.yang");
+        modules.add("/i2rs-topology.yang");
+        modules.add("/yang-ext.yang");
+        modules.add("/opendaylight-inventory.yang");
+        modules.add("/opendaylight-l2-types.yang");
+        modules.add("/ietf-yang-types.yang");
+        modules.add("/opendaylight-topology.yang");
+        modules.add("/opendaylight-meter-types.yang");
+        modules.add("/opendaylight-queue-types.yang");
+        modules.add("/opendaylight-port-types.yang");
+        modules.add("/opendaylight-match-types.yang");
+        modules.add("/opendaylight-action-types.yang");
+        modules.add("/opendaylight-group-types.yang");
+        modules.add("/opendaylight-flow-types.yang");
+        modules.add("/opendaylight-table-types.yang");
+        modules.add("/flow-node-inventory.yang");
         return parser.parseSources(Collections2.transform(modules, new Function<String, ByteSource>() {
             @Override
             public ByteSource apply(final String input) {
@@ -72,8 +95,7 @@ public class PathTranslatorTest {
                 };
             }
         }));
-      }
-
+    }
 
     @Before
     public void startup() throws URISyntaxException, ParseException, IOException, YangSyntaxErrorException {
@@ -84,10 +106,12 @@ public class PathTranslatorTest {
     }
 
     /**
-     * Test case: valid, legal path is passed to translate() method.
-     * Result of this method (YangInstanceIdentifier) should be equal to expected value.
-     * @throws URISyntaxException if URI is in incorrect format. Should not happen because URI in this test case
-     * is a constant
+     * Test case: valid, legal path is passed to translate() method. Result of
+     * this method (YangInstanceIdentifier) should be equal to expected value.
+     *
+     * @throws URISyntaxException
+     *             if URI is in incorrect format. Should not happen because URI
+     *             in this test case is a constant
      * @throws ParseException
      */
     @Test
@@ -99,53 +123,163 @@ public class PathTranslatorTest {
         AugmentationIdentifier augmentationIdentifier = new AugmentationIdentifier(childNames);
         YangInstanceIdentifier translate = pathTranslator.translate("network-topology-pcep:path-computation-client",
                 CorrelationItemEnum.Node, mockSchemaHolder, NTmodel);
-        YangInstanceIdentifier expectedIdentifier =
-                YangInstanceIdentifier.builder().node(augmentationIdentifier).node(qName).build();
-        Assert.assertEquals("Incorrect valid YangInstanceIdentifier",
-                expectedIdentifier.toString(), translate.toString());
+        YangInstanceIdentifier expectedIdentifier = YangInstanceIdentifier.builder().node(augmentationIdentifier)
+                .node(qName).build();
+        System.out.println(expectedIdentifier.toString());
+        Assert.assertEquals("Incorrect valid YangInstanceIdentifier", expectedIdentifier.toString(),
+                translate.toString());
+    }
+
+    @Test
+    public void testLegalPathNtTp() throws URISyntaxException, ParseException {
+        Set<QName> childNames = new HashSet<>();
+        QName qName = new QName(new URI("urn:TBD:params:xml:ns:yang:nt:l3-unicast-igp-topology?revision=2013-07-12"),
+                "igp-termination-point-attributes");
+        childNames.add(qName);
+        AugmentationIdentifier augmentationIdentifier = new AugmentationIdentifier(childNames);
+        YangInstanceIdentifier translate =
+                pathTranslator.translate("l3-unicast-igp-topology:igp-termination-point-attributes",
+                CorrelationItemEnum.TerminationPoint, mockSchemaHolder, NTmodel);
+        YangInstanceIdentifier expectedIdentifier = YangInstanceIdentifier.builder().node(augmentationIdentifier)
+                .node(qName).build();
+        System.out.println(expectedIdentifier.toString());
+        Assert.assertEquals("Incorrect valid YangInstanceIdentifier", expectedIdentifier.toString(),
+                translate.toString());
+    }
+
+    @Test
+    public void testLegalPathInventoryTp() throws URISyntaxException, ParseException {
+        Set<QName> childNames = new HashSet<>();
+        QName qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "port-number");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "hardware-address");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "configuration");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "advertised-features");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "name");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "queue");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "current-feature");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "supported");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "peer-features");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "current-speed");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "maximum-speed");
+        childNames.add(qName);
+
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "state");
+        childNames.add(qName);
+        AugmentationIdentifier augmentationIdentifier = new AugmentationIdentifier(childNames);
+        YangInstanceIdentifier translate = pathTranslator.translate("flow-node-inventory:state",
+                CorrelationItemEnum.TerminationPoint, mockSchemaHolder, INVmodel);
+        YangInstanceIdentifier expectedIdentifier = YangInstanceIdentifier.builder().node(augmentationIdentifier)
+                .node(qName).build();
+
+        String[] expectedSplit = expectedIdentifier.getPathArguments().get(0).toString().split(",");
+        String[] translatedSplit = translate.getPathArguments().get(0).toString().split(",");
+        Set<String> expectedNames = new HashSet<>();
+        Set<String> translatedNames = new HashSet<>();
+        for (int i = 0; i < expectedSplit.length; i++) {
+            expectedNames.add(expectedSplit[i].substring(expectedSplit[i].indexOf(')')).replaceAll("[^a-z]", ""));
+            translatedNames.add(translatedSplit[i].substring(translatedSplit[i].indexOf(')')).replaceAll("[^a-z]", ""));
+        }
+        Assert.assertEquals(expectedNames, translatedNames);
+    }
+
+    @Test
+    public void testLegalPathInventory() throws URISyntaxException, ParseException {
+        Set<QName> childNames = new HashSet<>();
+        QName qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "manufacturer");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "hardware");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "supported-actions");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "software");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "serial-number");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "description");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "table");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "meter");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "group");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "supported-match-types");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "supported-instructions");
+        childNames.add(qName);
+        qName = new QName(new URI("urn:opendaylight:flow:inventory?revision=2013-08-19"), "switch-features");
+        childNames.add(qName);
+        AugmentationIdentifier augmentationIdentifier = new AugmentationIdentifier(childNames);
+        YangInstanceIdentifier translate = pathTranslator.translate("flow-node-inventory:switch-features",
+                CorrelationItemEnum.Node, mockSchemaHolder, INVmodel);
+        YangInstanceIdentifier expectedIdentifier = YangInstanceIdentifier.builder().node(augmentationIdentifier)
+                .node(qName).build();
+
+        String[] expectedSplit = expectedIdentifier.getPathArguments().get(0).toString().split(",");
+        String[] translatedSplit = translate.getPathArguments().get(0).toString().split(",");
+        Set<String> expectedNames = new HashSet<>();
+        Set<String> translatedNames = new HashSet<>();
+        for (int i = 0; i < expectedSplit.length; i++) {
+            expectedNames.add(expectedSplit[i].substring(expectedSplit[i].indexOf(')')).replaceAll("[^a-z]", ""));
+            translatedNames.add(translatedSplit[i].substring(translatedSplit[i].indexOf(')')).replaceAll("[^a-z]", ""));
+        }
+        Assert.assertEquals(expectedNames, translatedNames);
     }
 
     /**
      * Test case: two colons in the path should not be accepted
      */
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testTwoColonsIllegalArgument() {
         pathTranslator.translate("network-topology-pcep::path-computation-client/network-topology-pcep:ip-address",
                 CorrelationItemEnum.Node, mockSchemaHolder, NTmodel);
     }
 
     /**
-     * Test case: the path should contain one and only one colon in one path argument
+     * Test case: the path should contain one and only one colon in one path
+     * argument
      */
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testNoColonsIllegalArgument() {
         pathTranslator.translate("network-topology-pceppath-computation-client/network-topology-pcep:ip-address",
                 CorrelationItemEnum.Node, mockSchemaHolder, NTmodel);
     }
 
     /**
-     * Test case: path in the argument does not match expected format [module name]:[child name]
+     * Test case: path in the argument does not match expected format [module
+     * name]:[child name]
      */
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testColonAtLastPosition() {
-        pathTranslator.translate("network-topology-pcep:/network-topology-pcep:ip-address",
-                CorrelationItemEnum.Node, mockSchemaHolder,NTmodel);
+        pathTranslator.translate("network-topology-pcep:/network-topology-pcep:ip-address", CorrelationItemEnum.Node,
+                mockSchemaHolder, NTmodel);
 
     }
 
     /**
-     * Test case: path in the argument does not match expected format [module name]:[child name]
+     * Test case: path in the argument does not match expected format [module
+     * name]:[child name]
      */
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testColonAtFirstPosition() {
-        pathTranslator.translate(":path-computation-client/network-topology-pcep:ip-address",
-                CorrelationItemEnum.Node, mockSchemaHolder, NTmodel);
+        pathTranslator.translate(":path-computation-client/network-topology-pcep:ip-address", CorrelationItemEnum.Node,
+                mockSchemaHolder, NTmodel);
     }
 
     /**
      * Test case: empty string as argument is not allowed
      */
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testEmptyString() {
         pathTranslator.translate("", CorrelationItemEnum.Node, mockSchemaHolder, NTmodel);
     }
@@ -153,39 +287,39 @@ public class PathTranslatorTest {
     /**
      * Test case: null as argument is not allowed
      */
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testYangPathNull() {
         pathTranslator.translate(null, CorrelationItemEnum.Node, mockSchemaHolder, NTmodel);
     }
 
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testPathBeginingWithSlash() {
-        pathTranslator.translate("/network-topology-pcep:ip-address",
-                CorrelationItemEnum.Node, mockSchemaHolder, NTmodel);
+        pathTranslator.translate("/network-topology-pcep:ip-address", CorrelationItemEnum.Node, mockSchemaHolder,
+                NTmodel);
     }
 
     /**
      * Test case: module does not exist
      */
-    @Test(expected=IllegalStateException.class)
+    @Test(expected = IllegalStateException.class)
     public void testUnexistingModuleName()
             throws URISyntaxException, ParseException, IOException, YangSyntaxErrorException {
         final YangParserImpl parser = new YangParserImpl();
         List<String> modules = new ArrayList<String>();
         modules.add("/ietf-inet-types.yang");
         modules.add("/network-topology.yang");
-        SchemaContext schemaContext2 = parser.parseSources(Collections2.transform(modules,
-                new Function<String, ByteSource>() {
-            @Override
-            public ByteSource apply(final String input) {
-                return new ByteSource() {
+        SchemaContext schemaContext2 = parser
+                .parseSources(Collections2.transform(modules, new Function<String, ByteSource>() {
                     @Override
-                    public InputStream openStream() throws IOException {
-                        return PathTranslatorTest.class.getResourceAsStream(input);
+                    public ByteSource apply(final String input) {
+                        return new ByteSource() {
+                            @Override
+                            public InputStream openStream() throws IOException {
+                                return PathTranslatorTest.class.getResourceAsStream(input);
+                            }
+                        };
                     }
-                };
-            }
-        }));
+                }));
         Mockito.when(mockSchemaHolder.getSchemaContext()).thenReturn(schemaContext2);
         testLegalPath();
     }
