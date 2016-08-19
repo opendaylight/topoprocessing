ec401b574fce9c679395ad783d528fe7c0643cfc
Martin Dindoffer <martin.dindoffer@pantheon.tech>
2016-08-19 12:06:40 +0200

    Migrate odl-topoprocessing-framework feature to blueprint
    * Got rid of DatastoreType enum
    * Changed the PingPongDataBroker dependency to using the DOMDataBroker interface
    * Adjusted tests
    
    Change-Id: I44ee07848fde3598f0a90062c3b364409615f4f3
    Signed-off-by: Martin Dindoffer <martin.dindoffer@pantheon.tech>

--- topoprocessing-inventory/src/test/java/org/opendaylight/topoprocessing/inventory/provider/InvTopoProcessingProviderImplTest.java
+++ topoprocessing-inventory/src/test/java/org/opendaylight/topoprocessing/inventory/provider/InvTopoProcessingProviderImplTest.java
@@ -8,38 +8,14 @@
 
 package org.opendaylight.topoprocessing.inventory.provider;
 
-import com.google.common.collect.SetMultimap;
-
-import java.net.URI;
-import java.util.Map;
-import java.util.Set;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.controller.md.sal.dom.api.DOMDataBroker;
-import org.opendaylight.controller.md.sal.dom.api.DOMDataChangeListener;
-import org.opendaylight.controller.md.sal.dom.api.DOMRpcProviderService;
-import org.opendaylight.controller.md.sal.dom.api.DOMRpcService;
-import org.opendaylight.controller.sal.core.api.model.SchemaService;
-import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
 import org.opendaylight.topoprocessing.impl.provider.TopoProcessingProviderImpl;
-import org.opendaylight.topoprocessing.impl.request.TopologyRequestListener;
-import org.opendaylight.topoprocessing.impl.rpc.RpcServices;
 import org.opendaylight.topoprocessing.inventory.adapter.InvModelAdapter;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topoprocessing.provider.impl.rev150209.DatastoreType;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.model.api.Module;
-import org.opendaylight.yangtools.yang.model.api.ModuleIdentifier;
-import org.opendaylight.yangtools.yang.model.api.SchemaContextListener;
-import org.opendaylight.yangtools.yang.model.util.AbstractSchemaContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.topology.correlation.rev150121.OpendaylightInventoryModel;
 
 /**
  * @author matus.marko
@@ -47,71 +23,14 @@ import org.opendaylight.yangtools.yang.model.util.AbstractSchemaContext;
 @RunWith(MockitoJUnitRunner.class)
 public class InvTopoProcessingProviderImplTest {
 
-    private TopoProcessingProviderImpl topoProcessingProvider;
-
-    @Mock private SchemaService schemaService;
-    @Mock private DOMDataBroker dataBroker;
-    @Mock private BindingNormalizedNodeSerializer nodeSerializer;
-    @Mock private RpcServices rpcServices;
-    @Mock private ListenerRegistration<SchemaContextListener> schemaContextListenerRegistration;
-    @Mock private ListenerRegistration<DOMDataChangeListener> topologyRequestListenerRegistration;
-    @Mock private InvModelAdapter mockInvModelAdapter;
-    @Mock private TopologyRequestListener mockTopoRequestListener;
-
-    private class SchemaContextTmp extends AbstractSchemaContext {
-
-        @Override
-        protected Map<ModuleIdentifier, String> getIdentifiersToSources() {
-            return null;
-        }
-
-        @Override
-        protected SetMultimap<URI, Module> getNamespaceToModules() {
-            return null;
-        }
-
-        @Override
-        protected SetMultimap<String, Module> getNameToModules() {
-            return null;
-        }
-
-        @Override
-        public Set<Module> getModules() {
-            return null;
-        }
-    }
-
-    @Before
-    public void setUp() {
-        Mockito.when(rpcServices.getRpcService()).thenReturn(Mockito.mock(DOMRpcService.class));
-        Mockito.when(rpcServices.getRpcProviderService()).thenReturn(Mockito.mock(DOMRpcProviderService.class));
-        SchemaContextTmp schemaContext = new SchemaContextTmp();
-        Mockito.when(schemaService.getGlobalContext()).thenReturn(schemaContext);
-    }
+    @Mock
+    private TopoProcessingProviderImpl mockTopoProcessingProvider;
 
     @Test
     public void testStartup() {
-        Mockito.when(schemaService.registerSchemaContextListener((SchemaContextListener) Matchers.any()))
-                .thenReturn(schemaContextListenerRegistration);
-        Mockito.when(dataBroker.registerDataChangeListener((LogicalDatastoreType) Matchers.any(),
-                (YangInstanceIdentifier) Matchers.any(), (DOMDataChangeListener) Matchers.any(),
-                (DataChangeScope) Matchers.any())).thenReturn(topologyRequestListenerRegistration);
-
-        // startup
-        topoProcessingProvider = new TopoProcessingProviderImpl(
-                schemaService, dataBroker, nodeSerializer, rpcServices, DatastoreType.OPERATIONAL);
-        topoProcessingProvider.startup();
         TopoProcessingProviderInv invProvider = new TopoProcessingProviderInv();
-        invProvider.startup(topoProcessingProvider);
-        Mockito.verify(schemaService).registerSchemaContextListener((SchemaContextListener) Matchers.any());
+        invProvider.startup(mockTopoProcessingProvider);
+        Mockito.verify(mockTopoProcessingProvider).registerModelAdapter(Mockito.eq(OpendaylightInventoryModel.class),
+                Mockito.any(InvModelAdapter.class));
     }
-
-    @Test
-    public void testClose() throws Exception {
-        testStartup();
-        // close
-        topoProcessingProvider.close();
-        Mockito.verify(schemaContextListenerRegistration).close();
-    }
-
 }
